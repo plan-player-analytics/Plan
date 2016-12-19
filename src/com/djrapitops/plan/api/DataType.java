bbc6b7f05a820c84a06ab35061fe5edf4d651f58
Rsl1122 <djrapitops@hotmail.com>
2016-12-19 23:36:12 +0200

    Added format of DataString for each DataType Enum

--- src/com/djrapitops/plan/api/DataType.java
+++ src/com/djrapitops/plan/api/DataType.java
@@ -2,5 +2,18 @@
 package com.djrapitops.plan.api;
 
 public enum DataType {
-    STRING, TIME, DATE, LOCATION, AMOUNT, AMOUNT_WITH_MAX, AMOUNT_WITH_LETTERS, BOOLEAN, PERCENT, OTHER, MAP, LINK, HEATMAP, DEPRECATED
+    STRING, // Any preformatted data & words
+    TIME, // Long in milliseconds
+    DATE, // Long in milliseconds since Epoch Date 1970
+    LOCATION, // X:# Y:# Z:#
+    AMOUNT, // Number
+    AMOUNT_WITH_MAX, // Example: 41 / 44
+    AMOUNT_WITH_LETTERS, // Example $50
+    BOOLEAN, // true/false
+    PERCENT, // Example 50%
+    OTHER, // Any data not listed here - will not be analyzed
+    MAP, // An image presentation of array in string format, no format yet
+    LINK, // Link to a webpage
+    HEATMAP, // An image presentation of array in string format, no format yet
+    DEPRECATED // Old data that has been rendered useless
 }
