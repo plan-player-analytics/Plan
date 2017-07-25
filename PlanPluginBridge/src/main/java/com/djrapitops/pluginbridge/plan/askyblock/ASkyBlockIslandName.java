7a4edcac03624deb49eb8151225154fb754c5f6e
Rsl1122 <djrapitops@hotmail.com>
2017-07-25 10:40:05 +0300

    Fix #168 Update included jar file to the fixed version

--- PlanPluginBridge/src/main/java/com/djrapitops/pluginbridge/plan/askyblock/ASkyBlockIslandName.java
+++ PlanPluginBridge/src/main/java/com/djrapitops/pluginbridge/plan/askyblock/ASkyBlockIslandName.java
@@ -1,9 +1,10 @@
 package com.djrapitops.pluginbridge.plan.askyblock;
 
 import com.wasteofplastic.askyblock.ASkyBlockAPI;
+import main.java.com.djrapitops.plan.data.additional.PluginData;
+
 import java.io.Serializable;
 import java.util.UUID;
-import main.java.com.djrapitops.plan.data.additional.PluginData;
 
 /**
  * PluginData class for ASkyBlock-plugin.
@@ -30,7 +31,7 @@ public class ASkyBlockIslandName extends PluginData {
     @Override
     public String getHtmlReplaceValue(String modifierPrefix, UUID uuid) {
         if (api.hasIsland(uuid)) {
-            return parseContainer(modifierPrefix, api.getIslandName(uuid) + "");
+            return parseContainer(modifierPrefix, api.getIslandName(uuid).replace("Â§r", ""));
         }
         return parseContainer(modifierPrefix, "No Island");
         
