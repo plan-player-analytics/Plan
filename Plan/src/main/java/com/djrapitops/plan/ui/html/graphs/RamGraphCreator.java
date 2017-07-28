36373968a772c3d611be63038bddf79556fc78cf
Rsl1122 <djrapitops@hotmail.com>
2017-07-28 14:42:11 +0300

    Add Chunk & Entity Graphs Remove New Players Graph from visualization
    Filter out 0s from some TPS data averages

--- Plan/src/main/java/com/djrapitops/plan/ui/html/graphs/RamGraphCreator.java
+++ Plan/src/main/java/com/djrapitops/plan/ui/html/graphs/RamGraphCreator.java
@@ -7,7 +7,22 @@ import main.java.com.djrapitops.plan.utilities.analysis.Point;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Class for creating scatter graph data from RAM Usage snapshots with TPS task.
+ *
+ * @author Rsl1122
+ * @since 3.6.0
+ * @see main.java.com.djrapitops.plan.data.listeners.TPSCountTimer
+ */
 public class RamGraphCreator {
+
+    /**
+     * Creates a scatter data string from given data.
+     *
+     * @param tpsData TPS Data collected by TPSCountTimer, one data point for each minute.
+     * @param scale Time span this graph resides within. (Milliseconds)
+     * @return Scatter Graph data string for ChartJs
+     */
     public static String buildScatterDataString(List<TPS> tpsData, long scale) {
         long now = MiscUtils.getTime();
         List<Point> points = tpsData.stream()
