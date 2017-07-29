a84632686db105941428ddd94dcb2f58bd2aba2a
Rsl1122 <djrapitops@hotmail.com>
2017-07-29 21:13:08 +0300

    Fix hashCode NPE Attempt to fix "issued command: /plan register" filtering from console output - failed

--- Plan/src/main/java/com/djrapitops/plan/command/commands/RegisterCommandFilter.java
+++ Plan/src/main/java/com/djrapitops/plan/command/commands/RegisterCommandFilter.java
@@ -1,7 +1,11 @@
 package main.java.com.djrapitops.plan.command.commands;
 
-import java.util.logging.Filter;
-import java.util.logging.LogRecord;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.message.Message;
 
 /**
  * Filters out WebUser registration command logs.
@@ -11,12 +15,126 @@ import java.util.logging.LogRecord;
  */
 public class RegisterCommandFilter implements Filter {
 
-    @Override
-    public boolean isLoggable(LogRecord record) {
-        String message = record.getMessage();
+    private boolean start = true;
+
+    private Result filter(String message) {
         boolean block = message.contains("command: /plan register")
                 || message.contains("command: /plan web register")
                 || message.contains("command: /plan webuser register");
-        return !block;
+        if (block) {
+            return Result.DENY;
+        }
+        return null;
+    }
+
+    @Override
+    public Result filter(LogEvent event) {
+        String message = event.getMessage().toString().toLowerCase();
+        return filter(message);
+    }
+
+    @Override
+    public Result filter(Logger arg0, Level arg1, Marker arg2, String arg3, Object... arg4) {
+        return filter(arg3);
+    }
+
+    @Override
+    public Result filter(Logger arg0, Level arg1, Marker arg2, Object arg3, Throwable arg4) {
+        return null;
+    }
+
+    @Override
+    public Result filter(Logger arg0, Level arg1, Marker arg2, Message arg3, Throwable arg4) {
+        return filter(arg3.toString());
+    }
+
+    @Override
+    public Result getOnMatch() {
+        return null;
+    }
+
+    @Override
+    public Result getOnMismatch() {
+        return null;
+    }
+
+    @Override
+    public Result filter(Logger logger, Level level, Marker marker, String s, Object o) {
+        return filter(s);
+    }
+
+    @Override
+    public Result filter(Logger logger, Level level, Marker marker, String s, Object o, Object o1) {
+        return filter(s);
+    }
+
+    @Override
+    public Result filter(Logger logger, Level level, Marker marker, String s, Object o, Object o1, Object o2) {
+        return filter(s);
+    }
+
+    @Override
+    public Result filter(Logger logger, Level level, Marker marker, String s, Object o, Object o1, Object o2, Object o3) {
+        return filter(s);
+    }
+
+    @Override
+    public Result filter(Logger logger, Level level, Marker marker, String s, Object o, Object o1, Object o2, Object o3, Object o4) {
+        return filter(s);
+    }
+
+    @Override
+    public Result filter(Logger logger, Level level, Marker marker, String s, Object o, Object o1, Object o2, Object o3, Object o4, Object o5) {
+        return filter(s);
+    }
+
+    @Override
+    public Result filter(Logger logger, Level level, Marker marker, String s, Object o, Object o1, Object o2, Object o3, Object o4, Object o5, Object o6) {
+        return filter(s);
+    }
+
+    @Override
+    public Result filter(Logger logger, Level level, Marker marker, String s, Object o, Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7) {
+        return filter(s);
+    }
+
+    @Override
+    public Result filter(Logger logger, Level level, Marker marker, String s, Object o, Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7, Object o8) {
+        return filter(s);
+    }
+
+    @Override
+    public Result filter(Logger logger, Level level, Marker marker, String s, Object o, Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7, Object o8, Object o9) {
+        return filter(s);
+    }
+
+    @Override
+    public State getState() {
+        return null;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void start() {
+        start = true;
+    }
+
+    @Override
+    public void stop() {
+        start = false;
+    }
+
+    @Override
+    public boolean isStarted() {
+        return start;
+    }
+
+    @Override
+    public boolean isStopped() {
+        return !start;
     }
 }
