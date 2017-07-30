30f8fd69e76e3c8884407c39f646f05a48f85af5
Rsl1122 <djrapitops@hotmail.com>
2017-07-30 12:00:21 +0300

    HighCharts Geolocation Map Attempts to fix error 12

--- Plan/src/main/java/com/djrapitops/plan/ui/html/graphs/SeriesCreator.java
+++ Plan/src/main/java/com/djrapitops/plan/ui/html/graphs/SeriesCreator.java
@@ -69,7 +69,7 @@ public class SeriesCreator {
             Point point = points.get(i);
             double y = point.getY();
             long date = (long) point.getX();
-            arrayBuilder.append("{x:").append(date).append(",y:").append(y).append("}");
+            arrayBuilder.append("[").append(date).append(",").append(y).append("]");
             if (i < size - 1) {
                 arrayBuilder.append(",");
             }
---
+++ Plan/src/main/java/com/djrapitops/plan/ui/html/graphs/WorldMapCreator.java
@@ -0,0 +1,58 @@
+package main.java.com.djrapitops.plan.ui.html.graphs;
+
+import java.util.Map;
+
+public class WorldMapCreator {
+
+    public static String createDataSeries(Map<String, Integer> geoCodeCounts) {
+        StringBuilder arrayBuilder = new StringBuilder("[");
+
+        int i = 0;
+        int size = geoCodeCounts.size();
+        for (Map.Entry<String, Integer> entry : geoCodeCounts.entrySet()) {
+            String geoCode = entry.getKey();
+            Integer players = entry.getValue();
+            if (players != 0) {
+                arrayBuilder.append("{'code':'").append(geoCode).append("','value':").append(players).append("}");
+                if (i < size - 1) {
+                    arrayBuilder.append(",");
+                }
+            }
+            i++;
+        }
+
+
+        arrayBuilder.append("]");
+        return arrayBuilder.toString();
+    }
+
+    public static String[] choroplethMapValues(Map<String, Integer> geoLocations, Map<String, String> geoCodes) {
+        StringBuilder locations = new StringBuilder("[");
+        StringBuilder z = new StringBuilder("[");
+        StringBuilder text = new StringBuilder("[");
+
+        int i = 0;
+        int size = geoLocations.size();
+        for (Map.Entry<String, Integer> entrySet : geoLocations.entrySet()) {
+            String country = entrySet.getKey();
+            String code = geoCodes.getOrDefault(country, "UNK");
+            int amount = entrySet.getValue();
+
+            z.append("\"").append(amount).append("\"");
+            locations.append("\"").append(country).append("\"");
+            text.append("\"").append(code).append("\"");
+
+            if (i < size - 1) {
+                locations.append(",");
+                z.append(",");
+                text.append(",");
+            }
+        }
+
+        locations.append("]");
+        z.append("]");
+        text.append("]");
+
+        return new String[]{z.toString(), locations.toString(), text.toString()};
+    }
+}
