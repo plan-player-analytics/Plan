ad2a3d80aedef065b2ae317fdc98e78d8ff131fd
Fuzzlemann <zhang.paul@ymail.com>
2017-08-15 13:04:59 +0200

    Change some PageCacheHandlerTest methods

--- Plan/src/test/java/main/java/com/djrapitops/plan/data/cache/PageCacheHandlerTest.java
+++ Plan/src/test/java/main/java/com/djrapitops/plan/data/cache/PageCacheHandlerTest.java
@@ -4,6 +4,7 @@ import main.java.com.djrapitops.plan.data.cache.PageCacheHandler;
 import main.java.com.djrapitops.plan.data.cache.PageLoader;
 import main.java.com.djrapitops.plan.ui.webserver.response.Response;
 import org.junit.Test;
+import test.java.utils.RandomData;
 
 import static junit.framework.TestCase.*;
 
@@ -11,13 +12,23 @@ import static junit.framework.TestCase.*;
  * @author Fuzzlemann
  */
 public class PageCacheHandlerTest {
-    private final String IDENTIFIER = "test";
-    private final PageLoader LOADER = () -> new Response() {
+    private final String IDENTIFIER = RandomData.randomString(10);
+    private final String RESPONSE_STRING = RandomData.randomString(10);
+    private final Response RESPONSE = new Response() {
         @Override
         public String getResponse() {
-            return "Test";
+            return RESPONSE_STRING;
         }
     };
+    private final PageLoader LOADER = () -> RESPONSE;
+
+    @Test
+    public void testCreateResponse() {
+        String expResponse = RESPONSE.getResponse();
+        String response = LOADER.createResponse().getResponse();
+
+        assertEquals(expResponse, response);
+    }
 
     @Test
     public void testCache() {
