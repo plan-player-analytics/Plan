plan-player-analytics/Plan

commit 0320b2cea83e626d06c16d13cd034d46d68d33ee

Removed TODOs from code that isn't maintained in this project.

Plan/src/main/resources/web/plugins/jquery-datatable/jquery.dataTables.js
@@ -55,7 +55,7 @@
 		// Browser
 		factory( jQuery, window, document );
 	}
-}
+};
 (function( $, window, document, undefined ) {
 	"use strict";
 
@@ -1054,7 +1054,6 @@
 			
 			var oClasses = oSettings.oClasses;
 			
-			// @todo Remove in 1.11
 			if ( oInit.bJQueryUI )
 			{
 				/* Use the JUI classes object for display. You could clone the oStdClasses object if
@@ -1225,7 +1224,6 @@
 			
 			/*
 			 * Sorting
-			 * @todo For modularisation (1.11) this needs to do into a sort start up handler
 			 */
 			
 			// If aaSorting is not defined, then we use the first indicator in asSorting
@@ -2861,10 +2859,6 @@
 	 * @param {int}    [colIdx] Column index to invalidate. If undefined the whole
 	 *     row will be invalidated
 	 * @memberof DataTable#oApi
-	 *
-	 * @todo For the modularisation of v1.11 this will need to become a callback, so
-	 *   the sort and filter methods can subscribe to it. That will required
-	 *   initialisation options for sorting, which is why it is not already baked in
 	 */
 	function _fnInvalidate( settings, rowIdx, src, colIdx )
 	{
@@ -3582,7 +3576,6 @@
 						j++;
 					}
 	
-					/* Replace jQuery UI constants @todo depreciated */
 					if ( sAttr == "H" )
 					{
 						sAttr = classes.sJUIHeader;
@@ -3621,7 +3614,6 @@
 				/* End container div */
 				insert = insert.parent();
 			}
-			// @todo Move options into their own plugins?
 			else if ( cOption == 'l' && features.bPaginate && features.bLengthChange )
 			{
 				/* Length */
@@ -4258,7 +4250,6 @@
 		};
 	
 		// Resolve any column types that are unknown due to addition or invalidation
-		// @todo As per sort - can this be moved into an event handler?
 		_fnColumnTypes( oSettings );
 	
 		/* In server-side processing all filtering is done by the server, so no point hanging around here */
@@ -5880,7 +5871,6 @@
 	 * Change the order of the table
 	 *  @param {object} oSettings dataTables settings object
 	 *  @memberof DataTable#oApi
-	 *  @todo This really needs split up!
 	 */
 	function _fnSort ( oSettings )
 	{
@@ -5898,7 +5888,6 @@
 			aSort;
 	
 		// Resolve any column types that are unknown due to addition or invalidation
-		// @todo Can this be moved into a 'data-ready' handler which is called when
 		//   data is going to be used in the table?
 		_fnColumnTypes( oSettings );
 	
@@ -6331,10 +6320,8 @@
 	
 		// Store the saved state so it might be accessed at any time
 		settings.oLoadedState = $.extend( true, {}, state );
-	
-		// Restore key features - todo - for 1.11 this needs to be done by
-		// subscribed events
-		if ( state.start !== undefined ) {
+
+        if ( state.start !== undefined ) {
 			settings._iDisplayStart    = state.start;
 			settings.iInitDisplayStart = state.start;
 		}
@@ -6485,7 +6472,6 @@
 	 *      references
 	 *  @returns {object} out Reference, just for convenience - out === the return.
 	 *  @memberof DataTable#oApi
-	 *  @todo This doesn't take account of arrays inside the deep copied objects.
 	 */
 	function _fnExtend( out, extender, breakRefs )
 	{
@@ -7164,7 +7150,6 @@
 	};
 	
 	
-	// @todo - Is there need for an augment function?
 	// _Api.augment = function ( inst, name )
 	// {
 	// 	// Find src object in the structure from the name
@@ -12866,13 +12851,6 @@
 	 * NOT be manipulated outside of DataTables. Any configuration should be done
 	 * through the initialisation options.
 	 *  @namespace
-	 *  @todo Really should attach the settings object to individual instances so we
-	 *    don't need to create new instances on each $().dataTable() call (if the
-	 *    table already exists). It would also save passing oSettings around and
-	 *    into every single function. However, this is a very significant
-	 *    architecture change for DataTables and will almost certainly break
-	 *    backwards compatibility with older installations. This is something that
-	 *    will be done in 2.0.
 	 */
 	DataTable.models.oSettings = {
 		/**
@@ -13195,7 +13173,6 @@
 		 * Note that this parameter will be set by the initialisation routine. To
 		 * set a default use {@link DataTable.defaults}.
 		 *  @type array
-		 *  @todo These inner arrays should really be objects
 		 */
 		"aaSorting": null,
 	

Plan/src/main/resources/web/plugins/jquery-steps/jquery.steps.js
@@ -597,8 +597,6 @@ function insertStep(wizard, options, state, index, step)
         throwError(_indexOutOfRangeErrorMessage);
     }
 
-    // TODO: Validate step object
-
     // Change data
     step = $.extend({}, stepModel, step);
     insertStepToCache(wizard, index, step);

Plan/src/main/resources/web/plugins/jquery/jquery.js
@@ -807,7 +807,6 @@ function Sizzle( selector, context, results, seed ) {
 						if ( (elem = context.getElementById( m )) ) {
 
 							// Support: IE, Opera, Webkit
-							// TODO: identify versions
 							// getElementById can match elements by name instead of ID
 							if ( elem.id === m ) {
 								results.push( elem );
@@ -821,7 +820,6 @@ function Sizzle( selector, context, results, seed ) {
 					} else {
 
 						// Support: IE, Opera, Webkit
-						// TODO: identify versions
 						// getElementById can match elements by name instead of ID
 						if ( newContext && (elem = newContext.getElementById( m )) &&
 							contains( context, elem ) &&

Plan/src/main/resources/web/plugins/momentjs/moment.js
@@ -873,7 +873,6 @@
             this._shortMonthsParse = [];
         }
 
-        // TODO: add sorting
         // Sorting makes sure if one month (or abbr) is a prefix of another
         // see sorting in computeMonthsParse
         for (i = 0; i < 12; i++) {
@@ -913,7 +912,6 @@
                 value = toInt(value);
             } else {
                 value = mom.localeData().monthsParse(value);
-                // TODO: Another silent failure?
                 if (typeof value !== 'number') {
                     return mom;
                 }
@@ -1756,7 +1754,6 @@
 
     function loadLocale(name) {
         var oldLocale = null;
-        // TODO: Find a better way to register and load all the locales in Node
         if (!locales[name] && (typeof module !== 'undefined') &&
                 module && module.exports) {
             try {
@@ -2120,10 +2117,6 @@
             dow = 1;
             doy = 4;
 
-            // TODO: We need to take the current isoWeekYear, but that depends on
-            // how we interpret now (local, utc, fixed offset). So create
-            // a now version of current config (take local/utc/offset flags, and
-            // create now).
             weekYear = defaults(w.GG, config._a[YEAR], weekOfYear(local__createLocal(), 1, 4).year);
             week = defaults(w.W, 1);
             weekday = defaults(w.E, 1);
@@ -2170,7 +2163,6 @@
 
     // date from string and format string
     function configFromStringAndFormat(config) {
-        // TODO: Move this to another part of the creation flow to prevent circular deps
         if (config._f === utils_hooks__hooks.ISO_8601) {
             configFromISO(config);
             return;
@@ -2462,7 +2454,6 @@
         return res;
     }
 
-    // TODO: Use [].sort instead?
     function min () {
         var args = [].slice.call(arguments, 0);
 
@@ -2842,7 +2833,6 @@
         }
     }
 
-    // TODO: remove 'name' arg after deprecation is removed
     function createAdder(direction, name) {
         return function (val, period) {
             var dur, tmp;
@@ -3924,7 +3914,6 @@
         }
     }
 
-    // TODO: Use this.as('ms')?
     function duration_as__valueOf () {
         return (
             this._milliseconds +

commit 311be89b0342f7c846fd738339573d4f6a9b7185

fixed a bug in jquery.dataTables.js introduced in 0320b2cea83e626d06c16d13cd034d46d68d33ee

Plan/src/main/resources/web/plugins/jquery-datatable/jquery.dataTables.js
@@ -55,7 +55,7 @@
 		// Browser
 		factory( jQuery, window, document );
 	}
-};
+}
 (function( $, window, document, undefined ) {
 	"use strict";
 