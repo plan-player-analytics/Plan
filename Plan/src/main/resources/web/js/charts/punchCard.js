cdbd8f9496b23cc97989665a5da60c5320e1cf62
Rsl1122 <djrapitops@hotmail.com>
2018-02-11 15:23:42 +0200

    Reversed PunchCard days: (Now top -> bottom = Monday -> Sunday)

--- Plan/src/main/resources/web/js/charts/punchCard.js
+++ Plan/src/main/resources/web/js/charts/punchCard.js
@@ -1,26 +1,30 @@
 function punchCard(id, punchcardSeries) {
-	Highcharts.chart(id, {
-		chart: {
-			defaultSeriesType: 'scatter'
-		},
-		title: {text: ''},
-		xAxis: {
-			type: 'datetime',
-			dateTimeLabelFormats: {
+    Highcharts.chart(id, {
+        chart: {
+            defaultSeriesType: 'scatter'
+        },
+        title: {text: ''},
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
                 hour: '%I %P',
                 day: '%H %P'
-			},
-			tickInterval: 3600000
-		},
-        time: {
-        	timezoneOffset: 0
+            },
+            tickInterval: 3600000
         },
-		yAxis: {
-			categories: ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-		},
-		tooltip: {
-			pointFormat: 'Activity: {point.z}'
-		},
-		series: [punchcardSeries]
-	});
+        time: {
+            timezoneOffset: 0
+        },
+        yAxis: {
+            title: {
+                text: "Day of the Week"
+            },
+            reversed: true,
+            categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        },
+        tooltip: {
+            pointFormat: 'Activity: {point.z}'
+        },
+        series: [punchcardSeries]
+    });
 }
