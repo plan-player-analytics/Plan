00754584b6f9cc1fc35e812bb56048e9be3caab1
Smile <smilesdcru@gmail.com>
2018-07-21 15:45:14 +0300

    ~

--- common/src/main/java/com/djrapitops/plan/system/processing/CriticalCallable.java
+++
@@ -1,6 +0,0 @@
-package com.djrapitops.plan.system.processing;
-
-import java.util.concurrent.Callable;
-
-public interface CriticalCallable<T> extends Callable<T> {
-}
--- common/src/main/java/com/djrapitops/plan/system/processing/CriticalRunnable.java
+++
@@ -1,4 +0,0 @@
-package com.djrapitops.plan.system.processing;
-
-public interface CriticalRunnable extends Runnable {
-}
