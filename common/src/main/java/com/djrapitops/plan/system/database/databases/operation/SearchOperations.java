3ec4dd0ad1d18ce143133cf15f86679b269bbc2a
Smile <smilesdcru@gmail.com>
2018-07-21 16:07:51 +0300

    ~

--- common/src/main/java/com/djrapitops/plan/system/database/databases/operation/SearchOperations.java
+++
@@ -1,9 +0,0 @@
-package com.djrapitops.plan.system.database.databases.operation;
-
-import java.util.List;
-
-public interface SearchOperations {
-
-    List<String> matchingPlayers(String search);
-
-}
