plan-player-analytics/Plan

commit fcc87d5a877d9e3e888441088d708d9ae62a9c17

TextStringBuilder to some appropriate instances

Plan/src/main/java/com/djrapitops/plan/utilities/html/graphs/WorldMap.java
@@ -1,6 +1,7 @@
 package com.djrapitops.plan.utilities.html.graphs;
 
 import com.djrapitops.plan.data.store.mutators.PlayersMutator;
+import org.apache.commons.text.TextStringBuilder;
 
 import java.util.HashMap;
 import java.util.List;
@@ -52,25 +53,15 @@ public class WorldMap implements HighChart {
             }
         }
 
-        StringBuilder dataBuilder = new StringBuilder("[");
+        TextStringBuilder dataBuilder = new TextStringBuilder("[");
 
-        int i = 0;
-        int size = geoCodeCounts.size();
-        for (Map.Entry<String, Integer> entry : geoCodeCounts.entrySet()) {
-            String geoCode = entry.getKey();
-            Integer players = entry.getValue();
+        dataBuilder.appendWithSeparators(
+                geoCodeCounts.entrySet().stream()
+                        .map(entry -> "{'code':'" + entry.getKey() + "','value':" + entry.getValue() + "}")
+                        .iterator(),
+                ","
+        );
 
-            if (players != 0) {
-                dataBuilder.append("{'code':'").append(geoCode).append("','value':").append(players).append("}");
-                if (i < size - 1) {
-                    dataBuilder.append(",");
-                }
-            }
-
-            i++;
-        }
-
-        dataBuilder.append("]");
-        return dataBuilder.toString();
+        return dataBuilder.append("]").toString();
     }
 }

Plan/src/main/java/com/djrapitops/plan/utilities/html/HtmlStructure.java
@@ -16,6 +16,7 @@ import com.djrapitops.plan.utilities.html.icon.Icon;
 import com.djrapitops.plan.utilities.html.icon.Icons;
 import com.djrapitops.plugin.api.utility.log.Log;
 import com.djrapitops.plugin.utilities.Verify;
+import org.apache.commons.text.TextStringBuilder;
 
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
@@ -28,18 +29,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class HtmlStructure {
 
     public static String separateWithDots(String... elements) {
-        if (elements.length == 0) {
-            return "";
-        }
-        StringBuilder builder = new StringBuilder(elements[0]);
-        for (int i = 1; i < elements.length; i++) {
-            String element = elements[i];
-            if (element.isEmpty()) {
-                continue;
-            }
-            builder.append(" &#x2022; ");
-            builder.append(element);
-        }
+        TextStringBuilder builder = new TextStringBuilder();
+        builder.appendWithSeparators(elements, " &#x2022; ");
         return builder.toString();
     }
 
@@ -168,22 +159,16 @@ public class HtmlStructure {
     public static String playerStatus(boolean online, boolean banned, boolean op) {
         StringBuilder html = new StringBuilder("<p>");
         if (online) {
-            html.append(Icon.called("circle").of(Color.GREEN))
-                    .append(" Online");
+            html.append(Icon.called("circle").of(Color.GREEN)).append(" Online");
         } else {
-            html.append(Icon.called("circle").of(Color.RED))
-                    .append(" Offline");
+            html.append(Icon.called("circle").of(Color.RED)).append(" Offline");
         }
         html.append("</p>");
         if (op) {
-            html.append("<p>")
-                    .append(Icons.OPERATOR)
-                    .append(" Operator</p>");
+            html.append("<p>").append(Icons.OPERATOR).append(" Operator</p>");
         }
         if (banned) {
-            html.append("<p>")
-                    .append(Icons.BANNED)
-                    .append(" Banned");
+            html.append("<p>").append(Icons.BANNED).append(" Banned</p>");
         }
         return html.toString();
     }

Plan/src/main/java/com/djrapitops/plan/utilities/html/graphs/bar/BarGraph.java
@@ -1,6 +1,7 @@
 package com.djrapitops.plan.utilities.html.graphs.bar;
 
 import com.djrapitops.plan.utilities.html.graphs.HighChart;
+import org.apache.commons.text.TextStringBuilder;
 
 import java.util.List;
 
@@ -13,35 +14,15 @@ public class BarGraph implements HighChart {
     }
 
     public String toHighChartsCategories() {
-        StringBuilder categories = new StringBuilder("[");
-
-        int i = 0;
-        int size = bars.size();
-        for (Bar bar : bars) {
-            categories.append("'").append(bar.getLabel()).append("'");
-            if (i < size - 1) {
-                categories.append(",");
-            }
-            i++;
-        }
-
+        TextStringBuilder categories = new TextStringBuilder("[");
+        categories.appendWithSeparators(bars.stream().map(bar -> "'" + bar.getLabel() + "'").iterator(), ",");
         return categories.append("]").toString();
     }
 
     @Override
     public String toHighChartsSeries() {
-        StringBuilder series = new StringBuilder("[");
-
-        int i = 0;
-        int size = bars.size();
-        for (Bar bar : bars) {
-            series.append(bar.getValue());
-            if (i < size - 1) {
-                series.append(",");
-            }
-            i++;
-        }
-
+        TextStringBuilder series = new TextStringBuilder("[");
+        series.appendWithSeparators(bars.stream().map(Bar::getValue).iterator(), ",");
         return series.append("]").toString();
     }
 }

Plan/src/main/java/com/djrapitops/plan/utilities/html/graphs/pie/AbstractPieChart.java
@@ -5,6 +5,7 @@
 package com.djrapitops.plan.utilities.html.graphs.pie;
 
 import com.djrapitops.plan.utilities.html.graphs.HighChart;
+import org.apache.commons.text.TextStringBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,19 +30,9 @@ public class AbstractPieChart implements HighChart {
 
     @Override
     public String toHighChartsSeries() {
-        StringBuilder seriesBuilder = new StringBuilder("[");
-        int i = 0;
-        int size = slices.size();
-        for (PieSlice slice : slices) {
-            seriesBuilder.append(slice.toString());
-            if (i < size - 1) {
-                seriesBuilder.append(",");
-            }
-            i++;
-        }
-        seriesBuilder.append("]");
-
-        return seriesBuilder.toString();
+        TextStringBuilder series = new TextStringBuilder("[");
+        series.appendWithSeparators(slices, ",");
+        return series.append("]").toString();
     }
 
     public void setSlices(List<PieSlice> slices) {

commit addd463406a63e41fec689d5b9647ffdfc81e853

Fixed World Map causing issues introduced in fcc87d5

Plan/src/main/java/com/djrapitops/plan/utilities/html/graphs/WorldMap.java
@@ -57,6 +57,7 @@ public class WorldMap implements HighChart {
 
         dataBuilder.appendWithSeparators(
                 geoCodeCounts.entrySet().stream()
+                        .filter(entry -> entry.getValue() != 0)
                         .map(entry -> "{'code':'" + entry.getKey() + "','value':" + entry.getValue() + "}")
                         .iterator(),
                 ","