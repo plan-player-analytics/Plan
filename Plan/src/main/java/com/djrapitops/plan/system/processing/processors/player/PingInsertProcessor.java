3afaad973124dda2365e9e98b4cbca39fe9af12f
Rsl1122 <djrapitops@hotmail.com>
2018-08-21 10:23:13 +0300

    Changed gathered ping average to be median instead #691 (For each datapoint a median is calculated, which is then used in calculation for mean values, averages)

--- Plan/src/main/java/com/djrapitops/plan/system/processing/processors/player/PingInsertProcessor.java
+++ Plan/src/main/java/com/djrapitops/plan/system/processing/processors/player/PingInsertProcessor.java
@@ -9,10 +9,12 @@ import com.djrapitops.plan.data.store.objects.DateObj;
 import com.djrapitops.plan.system.database.databases.Database;
 import com.djrapitops.plan.system.info.server.ServerInfo;
 import com.djrapitops.plan.system.processing.CriticalRunnable;
+import com.djrapitops.plan.utilities.analysis.Median;
 
 import java.util.List;
 import java.util.OptionalInt;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Processes 60s values of a Ping list.
@@ -44,23 +46,28 @@ public class PingInsertProcessor implements CriticalRunnable {
             return;
         }
 
-        int minValue = history.stream()
-                .mapToInt(DateObj::getValue)
-                .filter(i -> i > 0 && i < 4000)
-                .min().orElse(-1);
+        int minValue = getMinValue(history);
 
-        double avgValue = history.stream()
-                .mapToInt(DateObj::getValue)
-                .filter(i -> i > 0 && i < 4000)
-                .average().orElse(-1);
+        int meanValue = getMeanValue(history);
 
         int maxValue = max.getAsInt();
 
         Ping ping = new Ping(lastDate, ServerInfo.getServerUUID(),
                 minValue,
                 maxValue,
-                avgValue);
+                meanValue);
 
         Database.getActive().save().ping(uuid, ping);
+    }
+
+    int getMinValue(List<DateObj<Integer>> history) {
+        return history.stream()
+                .mapToInt(DateObj::getValue)
+                .filter(i -> i > 0 && i < 4000)
+                .min().orElse(-1);
+    }
+
+    int getMeanValue(List<DateObj<Integer>> history) {
+        return (int) Median.forInt(history.stream().map(DateObj::getValue).collect(Collectors.toList())).calculate();
     }
 }
