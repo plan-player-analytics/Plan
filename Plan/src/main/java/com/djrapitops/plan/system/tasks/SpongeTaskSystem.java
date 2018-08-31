8a4b23649d205454ef045ed1c6b9e9dc8f5e0e0a
BrainStone <yannick@tedworld.de>
2018-08-31 18:47:04 +0200

    PingCountTimerSponge

--- Plan/src/main/java/com/djrapitops/plan/system/tasks/SpongeTaskSystem.java
+++ Plan/src/main/java/com/djrapitops/plan/system/tasks/SpongeTaskSystem.java
@@ -1,7 +1,11 @@
 package com.djrapitops.plan.system.tasks;
 
 import com.djrapitops.plan.PlanSponge;
+import com.djrapitops.plan.system.settings.Settings;
+import com.djrapitops.plan.system.tasks.server.PingCountTimerSponge;
 import com.djrapitops.plan.system.tasks.server.SpongeTPSCountTimer;
+import com.djrapitops.plugin.api.TimeAmount;
+import com.djrapitops.plugin.task.RunnableFactory;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.scheduler.Task;
 
@@ -9,6 +13,20 @@ public class SpongeTaskSystem extends ServerTaskSystem {
 
     public SpongeTaskSystem(PlanSponge plugin) {
         super(plugin, new SpongeTPSCountTimer(plugin));
+    }
+    
+    @Override
+    public void enable() {
+        super.enable();
+        try {
+            PingCountTimerSponge pingCountTimer = new PingCountTimerSponge();
+            Sponge.getEventManager().registerListeners(plugin, pingCountTimer);
+            long startDelay = TimeAmount.SECOND.ticks() * (long) Settings.PING_SERVER_ENABLE_DELAY.getNumber();
+            RunnableFactory.createNew("PingCountTimer", pingCountTimer)
+                    .runTaskTimer(startDelay, PingCountTimerSponge.PING_INTERVAL);
+        } catch (ExceptionInInitializerError | NoClassDefFoundError ignore) {
+            // Running CraftBukkit
+        }
     }
 
     @Override
