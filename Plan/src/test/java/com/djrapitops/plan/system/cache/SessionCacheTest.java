baad1b4c4430acae8a55fa685947bfb0d505cf77
Rsl1122 <djrapitops@hotmail.com>
2018-10-13 10:58:00 +0300

    [Test] Fixed SessionCacheTest

--- Plan/src/test/java/com/djrapitops/plan/system/cache/SessionCacheTest.java
+++ Plan/src/test/java/com/djrapitops/plan/system/cache/SessionCacheTest.java
@@ -1,11 +1,10 @@
 package com.djrapitops.plan.system.cache;
 
 import com.djrapitops.plan.data.container.Session;
-import com.djrapitops.plan.system.database.databases.Database;
-import org.junit.*;
-import org.junit.rules.TemporaryFolder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import utilities.TestConstants;
-import utilities.mocks.SystemMockUtil;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -15,25 +14,15 @@ import static org.junit.Assert.assertTrue;
 
 public class SessionCacheTest {
 
-    @ClassRule
-    public static TemporaryFolder temporaryFolder = new TemporaryFolder();
-    private SessionCache sessionCache;
     private Session session;
     private final UUID uuid = TestConstants.PLAYER_ONE_UUID;
     private final UUID serverUUID = TestConstants.SERVER_UUID;
 
-    private Database database; // TODO
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-        SystemMockUtil.setUp(temporaryFolder.getRoot())
-                .enableProcessing();
-    }
-
     @Before
     public void setUp() {
-        sessionCache = new SessionCache(database);
         session = new Session(uuid, serverUUID, 12345L, "World1", "SURVIVAL");
+
+        SessionCache sessionCache = new SessionCache(null);
         sessionCache.cacheSession(uuid, session);
     }
 
@@ -44,7 +33,6 @@ public class SessionCacheTest {
 
     @Test
     public void testAtomity() {
-        SessionCache reloaded = new SessionCache(database);
         Optional<Session> cachedSession = SessionCache.getCachedSession(uuid);
         assertTrue(cachedSession.isPresent());
         assertEquals(session, cachedSession.get());
@@ -54,7 +42,7 @@ public class SessionCacheTest {
     public void testBungeeReCaching() {
         SessionCache cache = new ProxyDataCache(null, null);
         cache.cacheSession(uuid, session);
-        Session expected = new Session(uuid, TestConstants.SERVER_UUID, 0, "", "");
+        Session expected = new Session(uuid, serverUUID, 0, "", "");
         cache.cacheSession(uuid, expected);
 
         Optional<Session> result = SessionCache.getCachedSession(uuid);
