98aaff03284614d3073dda7cd85bed3d41c725b9
Rsl1122 <djrapitops@hotmail.com>
2018-10-23 15:12:45 +0300

    [New] Added Nginx HTTPS Override
    HTTPS routing override can be activated by setting keystore path to
    'nginx' - In this mode Plan uses HTTP server and gives out HTTPS links.
    
    Affected issues: #669

--- Plan/src/main/java/com/djrapitops/plan/system/webserver/WebServer.java
+++ Plan/src/main/java/com/djrapitops/plan/system/webserver/WebServer.java
@@ -117,6 +117,9 @@ public class WebServer implements SubSystem {
             if (!usingHttps) {
                 logger.log(L.INFO_COLOR, "§e" + locale.getString(PluginLang.WEB_SERVER_NOTIFY_HTTP_USER_AUTH));
                 server = HttpServer.create(new InetSocketAddress(config.getString(Settings.WEBSERVER_IP), port), 10);
+            } else if (server == null) {
+                logger.log(L.INFO_COLOR, "§eWebServer: Nginx HTTPS Override enabled. HTTP Server in use, make sure that your Nginx is routing with HTTPS and AlternativeIP.Link points to Nginx");
+                server = HttpServer.create(new InetSocketAddress(config.getString(Settings.WEBSERVER_IP), port), 10);
             }
             server.createContext("/", requestHandler);
 
@@ -138,6 +141,11 @@ public class WebServer implements SubSystem {
 
     private boolean startHttpsServer() {
         String keyStorePath = config.getString(Settings.WEBSERVER_CERTIFICATE_PATH);
+
+        if (keyStorePath.equalsIgnoreCase("nginx")) {
+            return true;
+        }
+
         if (!Paths.get(keyStorePath).isAbsolute()) {
             keyStorePath = files.getDataFolder() + File.separator + keyStorePath;
         }
