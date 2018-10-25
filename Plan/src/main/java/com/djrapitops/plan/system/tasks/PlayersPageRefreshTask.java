1d6a6b4b072c655471803df770186e4ab6ea0ec3
Rsl1122 <djrapitops@hotmail.com>
2018-10-25 10:38:17 +0300

    [Fix] Fix injection issue

--- Plan/src/main/java/com/djrapitops/plan/system/tasks/PlayersPageRefreshTask.java
+++ Plan/src/main/java/com/djrapitops/plan/system/tasks/PlayersPageRefreshTask.java
@@ -4,10 +4,16 @@ import com.djrapitops.plan.system.webserver.cache.PageId;
 import com.djrapitops.plan.system.webserver.cache.ResponseCache;
 import com.djrapitops.plugin.task.AbsRunnable;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class PlayersPageRefreshTask extends AbsRunnable {
+
+    @Inject
+    public PlayersPageRefreshTask() {
+    }
+
     @Override
     public void run() {
         ResponseCache.clearResponse(PageId.PLAYERS.id());
