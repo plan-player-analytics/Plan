cc1574ea0b1975436d5de5958fd205c88f58d7ad
Rsl1122 <djrapitops@hotmail.com>
2018-10-28 10:02:49 +0200

    [License] Updated license headers

--- Plan/src/main/java/com/djrapitops/plan/data/store/keys/AnalysisKeys.java
+++ Plan/src/main/java/com/djrapitops/plan/data/store/keys/AnalysisKeys.java
@@ -1,3 +1,19 @@
+/*
+ *  This file is part of Player Analytics (Plan).
+ *
+ *  Plan is free software: you can redistribute it and/or modify
+ *  it under the terms of the LGNU Lesser General Public License v3 as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Plan is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  LGNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Plan. If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.djrapitops.plan.data.store.keys;
 
 import com.djrapitops.plan.data.store.Key;
@@ -159,8 +175,7 @@ public class AnalysisKeys {
     public static final Key<Set<UUID>> BAN_DATA = new Key<>(new Type<Set<UUID>>() {}, "BAN_DATA");
     public static final Key<TreeMap<Long, Integer>> UNIQUE_PLAYERS_PER_DAY = new Key<>(new Type<TreeMap<Long, Integer>>() {}, "UNIQUE_PLAYERS_PER_DAY");
     public static final Key<TreeMap<Long, Integer>> NEW_PLAYERS_PER_DAY = new Key<>(new Type<TreeMap<Long, Integer>>() {}, "NEW_PLAYERS_PER_DAY");
-    
-    
+
     private AnalysisKeys() {
         /* Static variable class */
     }
