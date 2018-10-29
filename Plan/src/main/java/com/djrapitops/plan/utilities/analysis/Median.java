91d8731ab5b099a193cabdb58c01630683a835d1
Fuzzlemann <zhang.paul2003@gmail.com>
2018-10-29 13:42:50 +0100

    [Merge] Median and DecimalFormatter improvements (#765) by Fuzzlemann
    * Generified Median.java
    * Improves performance when comparing Integers because of Collection -> Stream -> List conversion removal. Instead of Median.forInt or Median.forLong Median.forList can be used.
    * Now every type of number can be compared
    * Storing DecimalFormat rather than creating a new DecimalFormat object each time it's applied
    * Added JavaDocs for Median.java

--- Plan/src/main/java/com/djrapitops/plan/utilities/analysis/Median.java
+++ Plan/src/main/java/com/djrapitops/plan/utilities/analysis/Median.java
@@ -16,37 +16,34 @@
  */
 package com.djrapitops.plan.utilities.analysis;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Math utility for calculating the median from Integer values.
  *
+ * @param <T> a {@code Number} object which implements {@code Comparable} (In general every standard Java number)
  * @author Rsl1122
  */
-public class Median {
+public class Median<T extends Number & Comparable<? super T>> {
 
-    private final List<Long> values;
+    private final List<T> values;
     private int size;
 
-    private Median(Collection<Integer> values, int b) {
-        this(values.stream().map(i -> (long) i).collect(Collectors.toList()));
-    }
-
-    private Median(List<Long> values) {
+    private Median(List<T> values) {
         this.values = values;
         Collections.sort(values);
         size = values.size();
     }
 
-    public static Median forInt(Collection<Integer> integers) {
-        return new Median(integers, 0);
-    }
-
-    public static Median forLong(List<Long> longs) {
-        return new Median(longs);
+    /**
+     * Creates a Median instance
+     *
+     * @param list the input list
+     * @return an instance of {@code Median} for the List given
+     */
+    public static <T extends Number & Comparable<? super T>> Median<T> forList(List<T> list) {
+        return new Median<>(list);
     }
 
     public double calculate() {
@@ -62,13 +59,13 @@ public class Median {
 
     private double calculateEven() {
         int half = size / 2;
-        double x1 = values.get(half);
-        double x2 = values.get(half - 1);
+        double x1 = values.get(half).doubleValue();
+        double x2 = values.get(half - 1).doubleValue();
         return (x1 + x2) / 2;
     }
 
     private double calculateOdd() {
         int half = size / 2;
-        return (double) values.get(half);
+        return values.get(half).doubleValue();
     }
 }
