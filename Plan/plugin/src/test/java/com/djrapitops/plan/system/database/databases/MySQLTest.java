5403f78a945f531c400e0214984703037cd68182
Rsl1122 <djrapitops@hotmail.com>
2018-12-26 21:44:59 +0200

    Removed failing test in MySQLTest

--- Plan/plugin/src/test/java/com/djrapitops/plan/system/database/databases/MySQLTest.java
+++ Plan/plugin/src/test/java/com/djrapitops/plan/system/database/databases/MySQLTest.java
@@ -3,10 +3,8 @@ package com.djrapitops.plan.system.database.databases;
 import com.djrapitops.plan.system.settings.config.PlanConfig;
 import com.djrapitops.plan.system.settings.paths.DatabaseSettings;
 import org.junit.BeforeClass;
-import org.junit.Test;
 import utilities.CIProperties;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
 /**
@@ -32,10 +30,5 @@ public class MySQLTest extends CommonDBTest {
         config.set(DatabaseSettings.TYPE, "MySQL");
 
         handleSetup("MySQL");
-    }
-
-    @Test
-    public void failingTest() {
-        assertTrue(false);
     }
 }
