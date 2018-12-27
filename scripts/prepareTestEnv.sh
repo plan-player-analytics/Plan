94e62cb63c6ee0b36613acfffd1db1f43d27c394
Risto Lahtela <djrapitops@hotmail.com>
2018-12-27 19:55:11 +0200

    Update prepareTestEnv.sh
    Added permissions for the chrome folders in an attempt to fix 
    org.openqa.selenium.WebDriverException: unknown error: Chrome failed to start: exited abnormally

--- scripts/prepareTestEnv.sh
+++ scripts/prepareTestEnv.sh
@@ -2,8 +2,12 @@
 wget -N http://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
+sudo chmod -R u=rwx,g=rwx /usr/bin/google-chrome-stable 
+sudo chmod -R u=rwx,g=rwx /usr/bin/X11/google-chrome-stable
+
 sudo mv -f ~/chromedriver /usr/local/share/
-sudo chmod +x /usr/local/share/chromedriver
+sudo chmod u=rwx,g=rwx /usr/local/share/chromedriver
+
 sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
 
 whereis google-chrome-stable
