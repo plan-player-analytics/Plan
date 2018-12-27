73a15e567a8e48addf2036eb0add9fff97459577
Risto Lahtela <djrapitops@hotmail.com>
2018-12-27 20:44:04 +0200

    Update prepareTestEnv.sh
    Added xvfb screen display settings

--- scripts/prepareTestEnv.sh
+++ scripts/prepareTestEnv.sh
@@ -1,3 +1,8 @@
+# Start Xvfb screen
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+
 # Install chromedriver
 wget -N http://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
