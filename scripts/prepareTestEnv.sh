ac6ae09a059d81a7e9e20e62bb2d12f921a4ebcf
Risto Lahtela <djrapitops@hotmail.com>
2018-12-27 21:06:10 +0200

    Ci selenium fix (#863)
    * Added --no-sandbox argument to chrome on travis
    * Added xvfb screen display settings
    * Updated chromedriver version

--- scripts/prepareTestEnv.sh
+++ scripts/prepareTestEnv.sh
@@ -1,5 +1,10 @@
+# Start Xvfb screen
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+
 # Install chromedriver
-wget -N http://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip -P ~/
+wget -N https://chromedriver.storage.googleapis.com/2.45/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
 sudo chmod -R u=rwx,g=rwx /usr/bin/google-chrome-stable 
