749880bee3420b221d5401124f7ba71661fbd9c3
Rsl1122 <djrapitops@hotmail.com>
2019-01-01 11:31:27 +0200

    update clear tables in MySQLTest

--- Plan/plugin/src/test/java/com/djrapitops/plan/system/database/databases/sql/MySQLTest.java
+++ Plan/plugin/src/test/java/com/djrapitops/plan/system/database/databases/sql/MySQLTest.java
@@ -1,16 +1,9 @@
 package com.djrapitops.plan.system.database.databases.sql;
 
-import com.djrapitops.plan.system.database.databases.sql.patches.Patch;
-import com.djrapitops.plan.system.database.databases.sql.processing.QueryAllStatement;
 import com.djrapitops.plan.system.settings.config.PlanConfig;
 import com.djrapitops.plan.system.settings.paths.DatabaseSettings;
 import org.junit.BeforeClass;
 import utilities.CIProperties;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assume.assumeTrue;
 
@@ -41,30 +34,8 @@ public class MySQLTest extends CommonDBTest {
     }
 
     private static void clearDatabase() {
-        List<String> tables = db.query(new QueryAllStatement<List<String>>("SELECT table_name" +
-                " FROM information_schema.tables") {
-            @Override
-            public List<String> processResults(ResultSet resultSet) throws SQLException {
-                List<String> names = new ArrayList<>();
-                while (resultSet.next()) {
-                    names.add(resultSet.getString("table_name"));
-                }
-                return names;
-            }
-        });
-
-        new Patch(db) {
-            @Override
-            public boolean hasBeenApplied() {
-                return false;
-            }
-
-            @Override
-            public void apply() {
-                for (String tableName : tables) {
-                    dropTable(tableName);
-                }
-            }
-        }.apply();
+        db.execute("DROP DATABASE Plan");
+        db.execute("CREATE DATABASE Plan");
+        db.execute("USE Plan");
     }
 }
