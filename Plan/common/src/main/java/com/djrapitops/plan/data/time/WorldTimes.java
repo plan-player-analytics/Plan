2fc8c7c29b03866ec5d2b9a48f483f03a9b29455
Rsl1122 <djrapitops@hotmail.com>
2019-02-25 10:36:53 +0200

    [#930] Null check to WorldTimes#updateState

--- Plan/common/src/main/java/com/djrapitops/plan/data/time/WorldTimes.java
+++ Plan/common/src/main/java/com/djrapitops/plan/data/time/WorldTimes.java
@@ -77,6 +77,8 @@ public class WorldTimes {
      * @param changeTime Epoch ms the change occurred.
      */
     public void updateState(String worldName, String gameMode, long changeTime) {
+        if (worldName == null || gameMode == null) return;
+
         GMTimes currentGMTimes = times.get(currentWorld);
         if (currentWorld.equals(worldName)) {
             currentGMTimes.changeState(gameMode, changeTime);
