40075f697cf4444ca72bbc1b743d81d2373c2ebd
Rsl1122 <djrapitops@hotmail.com>
2019-03-26 14:16:17 +0200

    Nest query since MySQL can not select and update:
    More information
    https://stackoverflow.com/a/9843719
    
    Attempts to fix:
    'You can't specify target table 'plan_extension_user_values' for update
    in FROM clause'

--- Plan/common/src/main/java/com/djrapitops/plan/extension/implementation/storage/transactions/results/RemoveUnsatisfiedConditionalResultsTransaction.java
+++ Plan/common/src/main/java/com/djrapitops/plan/extension/implementation/storage/transactions/results/RemoveUnsatisfiedConditionalResultsTransaction.java
@@ -92,7 +92,7 @@ public class RemoveUnsatisfiedConditionalResultsTransaction extends Transaction 
                 AND + ExtensionProviderTable.CONDITION + " IS NOT NULL"; // Ignore values that don't need condition
 
         String sql = "DELETE FROM " + playerValueTable +
-                WHERE + ExtensionPlayerValueTable.ID + " IN (" + selectUnsatisfiedValueIDs + ')';
+                WHERE + ExtensionPlayerValueTable.ID + " IN (" + SELECT + ExtensionPlayerValueTable.ID + FROM + '(' + selectUnsatisfiedValueIDs + "))";
 
         return new ExecStatement(sql) {
             @Override
