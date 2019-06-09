e4ae8c264438c146cec171f5b5fdcbbaaab3f35b
Rsl1122 <djrapitops@hotmail.com>
2019-06-09 11:33:37 +0300

    Added chart files and tested players chart

---
+++ Plan/common/src/main/resources/assets/plan/web/js/charts/activityPie.js
@@ -0,0 +1,25 @@
+function activityPie(id, activitySeries) {
+    Highcharts.chart(id, {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {text: ''},
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.y}</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [activitySeries]
+    });
+}
