1965a1ef8eac4351811ab24b0daa8067f7553300
Rsl1122 <djrapitops@hotmail.com>
2019-07-06 11:48:02 +0300

    Calendar night mode & text color fix

--- Plan/common/src/main/resources/assets/plan/web/js/color-selector.js
+++ Plan/common/src/main/resources/assets/plan/web/js/color-selector.js
@@ -79,8 +79,9 @@
                 '.card,.bg-white,.modal-content,.page-loader,hr {background-color:#44475a;border-color:#6272a4;}' +
                 '.bg-white.collapse-inner {border 1px solid;}' +
                 '.card-header {background-color:#44475a;border-color:#6272a4;}' +
-                '#content,.col-black,.text-gray-800,.collapse-item,.modal-title,.modal-body,.page-loader,.close {color:#eee8d5 !important;}' +
+                '#content,.col-black,.text-gray-800,.collapse-item,.modal-title,.modal-body,.page-loader,.close,.fc-title {color:#eee8d5 !important;}' +
                 '.collapse-item:hover,.nav-link.active {background-color: #606270 !important;}' +
+                '.fc-today {background:#6272a4 !important}' +
                 '</style>');
             // Turn bright tables to dark
             $('.table').addClass('table-dark');
