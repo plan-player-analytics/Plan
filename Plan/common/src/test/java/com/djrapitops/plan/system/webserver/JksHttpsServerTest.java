a129c24e7afcbaf8ea8be54c939ba8b91bcec9b7
Rsl1122 <djrapitops@hotmail.com>
2019-08-08 10:13:43 +0300

    Added PKCS12 support
    Affects issues:
    - Close #1102

--- Plan/common/src/test/java/com/djrapitops/plan/system/webserver/JksHttpsServerTest.java
+++ Plan/common/src/test/java/com/djrapitops/plan/system/webserver/JksHttpsServerTest.java
@@ -16,53 +16,38 @@
  */
 package com.djrapitops.plan.system.webserver;
 
-import com.djrapitops.plan.api.exceptions.connection.*;
 import com.djrapitops.plan.data.WebUser;
 import com.djrapitops.plan.db.access.transactions.commands.RegisterWebUserTransaction;
 import com.djrapitops.plan.system.PlanSystem;
 import com.djrapitops.plan.system.settings.config.PlanConfig;
 import com.djrapitops.plan.system.settings.paths.WebserverSettings;
-import com.djrapitops.plan.utilities.Base64Util;
 import com.djrapitops.plan.utilities.PassEncryptUtil;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
-import utilities.HTTPConnector;
 import utilities.RandomData;
 import utilities.TestResources;
 import utilities.mocks.PluginMockComponent;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.nio.file.Path;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-
-import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnitPlatform.class)
-class HTTPSWebServerAuthTest {
+class JksHttpsServerTest implements HttpsServerTest {
 
     private static final int TEST_PORT_NUMBER = RandomData.randomInt(9005, 9500);
 
-    public static PluginMockComponent component;
-
     private static PlanSystem system;
-
-    private HTTPConnector connector = new HTTPConnector();
 
     @BeforeAll
     static void setUpClass(@TempDir Path tempDir) throws Exception {
-        File file = tempDir.resolve("Cert.keystore").toFile();
-        TestResources.copyResourceIntoFile(file, "/Cert.keystore");
+        File file = tempDir.resolve("PlanCert.jks").toFile();
+        TestResources.copyResourceIntoFile(file, "/PlanCert.jks");
         String absolutePath = file.getAbsolutePath();
 
-        component = new PluginMockComponent(tempDir);
+        PluginMockComponent component = new PluginMockComponent(tempDir);
         system = component.getPlanSystem();
 
         PlanConfig config = system.getConfigSystem().getConfig();
@@ -87,38 +72,13 @@ class HTTPSWebServerAuthTest {
         }
     }
 
-    /**
-     * Test case against "Perm level 0 required, got 0".
-     */
-    @Test
-    void testHTTPSAuthForPages() throws IOException, WebException, KeyManagementException, NoSuchAlgorithmException {
-        assertTrue("WebServer is not using https", system.getWebServerSystem().getWebServer().isUsingHTTPS());
+    @Override
+    public WebServer getWebServer() {
+        return system.getWebServerSystem().getWebServer();
+    }
 
-        String address = "https://localhost:" + TEST_PORT_NUMBER;
-        URL url = new URL(address);
-        HttpURLConnection connection = connector.getConnection("GET", address);
-
-        String user = Base64Util.encode("test:testPass");
-        connection.setRequestProperty("Authorization", "Basic " + user);
-
-        int responseCode = connection.getResponseCode();
-
-        switch (responseCode) {
-            case 200:
-            case 302:
-                return;
-            case 400:
-                throw new BadRequestException("Bad Request: " + url.toString());
-            case 403:
-                throw new ForbiddenException(url.toString() + " returned 403");
-            case 404:
-                throw new NotFoundException(url.toString() + " returned a 404, ensure that your server is connected to an up to date Plan server.");
-            case 412:
-                throw new UnauthorizedServerException(url.toString() + " reported that it does not recognize this server. Make sure '/plan m setup' was successful.");
-            case 500:
-                throw new InternalErrorException();
-            default:
-                throw new WebException(url.toString() + "| Wrong response code " + responseCode);
-        }
+    @Override
+    public int testPortNumber() {
+        return TEST_PORT_NUMBER;
     }
 }
