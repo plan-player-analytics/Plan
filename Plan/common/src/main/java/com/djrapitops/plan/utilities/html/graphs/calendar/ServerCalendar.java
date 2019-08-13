f416f5ba6d9456be7f5ac68f43fce4db85dfb09e
Rsl1122 <djrapitops@hotmail.com>
2019-08-13 14:18:29 +0300

    Unique players / day query

--- Plan/common/src/main/java/com/djrapitops/plan/utilities/html/graphs/calendar/ServerCalendar.java
+++ Plan/common/src/main/java/com/djrapitops/plan/utilities/html/graphs/calendar/ServerCalendar.java
@@ -33,8 +33,8 @@ import java.util.*;
 public class ServerCalendar {
 
     private final PlayersMutator mutator;
-    private final TreeMap<Long, Integer> uniquePerDay;
-    private final TreeMap<Long, Integer> newPerDay;
+    private final SortedMap<Long, Integer> uniquePerDay;
+    private final SortedMap<Long, Integer> newPerDay;
 
     private final Formatter<Long> iso8601Formatter;
     private final Formatter<Long> timeAmountFormatter;
@@ -42,7 +42,7 @@ public class ServerCalendar {
     private final TimeZone timeZone;
 
     ServerCalendar(
-            PlayersMutator mutator, TreeMap<Long, Integer> uniquePerDay, TreeMap<Long, Integer> newPerDay,
+            PlayersMutator mutator, SortedMap<Long, Integer> uniquePerDay, SortedMap<Long, Integer> newPerDay,
             Formatter<Long> iso8601Formatter,
             Formatter<Long> timeAmountFormatter,
             Theme theme,
