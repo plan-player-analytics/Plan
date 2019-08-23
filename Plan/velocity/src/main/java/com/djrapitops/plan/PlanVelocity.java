cdb13e3b663b18e3938ad5efc60d165fa9301f6e
Rsl1122 <djrapitops@hotmail.com>
2019-08-23 17:21:12 +0300

    5.0 ALPHA build %placeholder%

--- Plan/velocity/src/main/java/com/djrapitops/plan/PlanVelocity.java
+++ Plan/velocity/src/main/java/com/djrapitops/plan/PlanVelocity.java
@@ -46,7 +46,7 @@ import java.nio.file.Path;
 @Plugin(
         id = "plan",
         name = "Plan",
-        version = "4.9.4",
+        version = "5.0 ALPHA build %buildNumber%",
         description = "Player Analytics Plugin by Rsl1122",
         authors = {"Rsl1122"}
 )
