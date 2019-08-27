539d817bf93805ab0144989149d9780a9e73e69e
Rsl1122 <djrapitops@hotmail.com>
2019-08-27 15:41:59 +0300

    Removed todos from library javascript

--- Plan/common/src/main/resources/assets/plan/web/vendor/bootstrap/js/bootstrap.js
+++ Plan/common/src/main/resources/assets/plan/web/vendor/bootstrap/js/bootstrap.js
@@ -176,7 +176,6 @@
         triggerTransitionEnd: function triggerTransitionEnd(element) {
             $(element).trigger(TRANSITION_END);
         },
-        // TODO: Remove in v5
         supportsTransitionEnd: function supportsTransitionEnd() {
             return Boolean(TRANSITION_END);
         },
@@ -2417,7 +2416,6 @@
                 }
             }; // ----------------------------------------------------------------------
             // the following methods are used to handle overflowing modals
-            // todo (fat): these should probably be refactored out of modal.js
             // ----------------------------------------------------------------------
 
 
@@ -3561,7 +3559,6 @@
                         var targetBCR = target.getBoundingClientRect();
 
                         if (targetBCR.width || targetBCR.height) {
-                            // TODO (fat): remove sketch reliance on jQuery position/offset
                             return [$(target)[offsetMethod]().top + offsetBase, targetSelector];
                         }
                     }
