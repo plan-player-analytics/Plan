dcb675e0109bd5301c9a8ea16a5f25bae636b298
Rsl1122 <djrapitops@hotmail.com>
2019-08-30 11:06:08 +0300

    utilities.file -> system.settings.upkeep

--- Plan/common/src/main/java/com/djrapitops/plan/system/settings/upkeep/FileWatcher.java
+++ Plan/common/src/main/java/com/djrapitops/plan/system/settings/upkeep/FileWatcher.java
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with Plan. If not, see <https://www.gnu.org/licenses/>.
  */
-package com.djrapitops.plan.utilities.file;
+package com.djrapitops.plan.system.settings.upkeep;
 
 import com.djrapitops.plugin.logging.L;
 import com.djrapitops.plugin.logging.error.ErrorHandler;
--- Plan/common/src/test/java/com/djrapitops/plan/system/settings/upkeep/FileWatcherTest.java
+++ Plan/common/src/test/java/com/djrapitops/plan/system/settings/upkeep/FileWatcherTest.java
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with Plan. If not, see <https://www.gnu.org/licenses/>.
  */
-package com.djrapitops.plan.utilities.file;
+package com.djrapitops.plan.system.settings.upkeep;
 
 import com.jayway.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeEach;
