1257401ee03077153bc65ae53bd8bbd8d033c569
Rsl1122 <24460436+Rsl1122@users.noreply.github.com>
2020-07-13 21:36:57 +0300

    Implemented Bungee command adapters
    - Fixed checkstyle errors
    - planbungee & planvelocity now aliases to new
      'planproxy' command

---
+++ Plan/bungeecord/src/main/java/com/djrapitops/plan/command/use/BungeeCMDSender.java
@@ -0,0 +1,66 @@
+/*
+ *  This file is part of Player Analytics (Plan).
+ *
+ *  Plan is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License v3 as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Plan is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Plan. If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.djrapitops.plan.command.use;
+
+import com.djrapitops.plan.commands.use.CMDSender;
+import com.djrapitops.plan.commands.use.ChatFormatter;
+import com.djrapitops.plan.commands.use.ConsoleChatFormatter;
+import com.djrapitops.plan.commands.use.MessageBuilder;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class BungeeCMDSender implements CMDSender {
+
+    CommandSender sender;
+
+    public BungeeCMDSender(CommandSender sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public Optional<String> getPlayerName() {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean hasPermission(String permission) {
+        return sender.hasPermission(permission);
+    }
+
+    @Override
+    public Optional<UUID> getUUID() {
+        return Optional.empty();
+    }
+
+    @Override
+    public MessageBuilder buildMessage() {
+        return new BungeePartBuilder(this);
+    }
+
+    @Override
+    public void send(String message) {
+        sender.sendMessage(new TextComponent(message));
+    }
+
+    @Override
+    public ChatFormatter getFormatter() {
+        return new ConsoleChatFormatter();
+    }
+}
---
+++ Plan/bungeecord/src/main/java/com/djrapitops/plan/command/use/BungeeCommand.java
@@ -0,0 +1,56 @@
+/*
+ *  This file is part of Player Analytics (Plan).
+ *
+ *  Plan is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License v3 as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Plan is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Plan. If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.djrapitops.plan.command.use;
+
+import com.djrapitops.plan.commands.use.Arguments;
+import com.djrapitops.plan.commands.use.Subcommand;
+import com.djrapitops.plugin.task.AbsRunnable;
+import com.djrapitops.plugin.task.RunnableFactory;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class BungeeCommand extends Command {
+
+    private final RunnableFactory runnableFactory;
+    private final Subcommand command;
+
+    public BungeeCommand(
+            RunnableFactory runnableFactory,
+            Subcommand command,
+            String name
+    ) {
+        super(name);
+        this.runnableFactory = runnableFactory;
+
+        this.command = command;
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        runnableFactory.create("", new AbsRunnable() {
+            @Override
+            public void run() {
+                if (sender instanceof ProxiedPlayer) {
+                    command.getExecutor().accept(new BungeePlayerCMDSender((ProxiedPlayer) sender), new Arguments(args));
+                } else {
+                    command.getExecutor().accept(new BungeeCMDSender(sender), new Arguments(args));
+                }
+            }
+        }).runTaskAsynchronously();
+    }
+}
