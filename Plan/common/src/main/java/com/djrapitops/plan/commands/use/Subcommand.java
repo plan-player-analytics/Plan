2cf7725b586f92faeff9fcbe18c970d8b31a7c3e
Risto Lahtela <24460436+Rsl1122@users.noreply.github.com>
2020-09-21 15:20:55 +0300

    Fixed NPE on bungee tab completion sometimes

--- Plan/common/src/main/java/com/djrapitops/plan/commands/use/Subcommand.java
+++ Plan/common/src/main/java/com/djrapitops/plan/commands/use/Subcommand.java
@@ -71,7 +71,7 @@ public class Subcommand {
     }
 
     public BiFunction<CMDSender, Arguments, List<String>> getArgumentResolver() {
-        return argumentResolver;
+        return argumentResolver != null ? argumentResolver : ((sender, arguments) -> Collections.emptyList());
     }
 
     public String getArgumentsAsString() {
