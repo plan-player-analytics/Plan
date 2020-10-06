6d47817418f51c76f2e6dd1f8b137e836f605b27
Risto Lahtela <24460436+Rsl1122@users.noreply.github.com>
2020-10-06 10:21:46 +0300

    Registered FactionsUUID Extension

--- Plan/extensions/src/main/java/com/djrapitops/plan/extension/implementation/ExtensionRegister.java
+++ Plan/extensions/src/main/java/com/djrapitops/plan/extension/implementation/ExtensionRegister.java
@@ -69,6 +69,7 @@ public class ExtensionRegister {
         register(new DKCoinsExtensionFactory(), DKCoinsExtensionFactory::createExtension, DKCoinsExtensionFactory::registerListener);
         register(new EssentialsExtensionFactory(), EssentialsExtensionFactory::createExtension, EssentialsExtensionFactory::registerUpdateListeners);
         register(new FactionsExtensionFactory(), FactionsExtensionFactory::createExtension);
+        register(new FactionsUUIDExtensionFactory(), FactionsUUIDExtensionFactory::createExtension, FactionsUUIDExtensionFactory::registerExpansion);
         register(new GriefDefenderExtensionFactory(), GriefDefenderExtensionFactory::createExtension);
         register(new GriefPreventionExtensionFactory(), GriefPreventionExtensionFactory::createExtension);
         register(new GriefPreventionSpongeExtensionFactory(), GriefPreventionSpongeExtensionFactory::createExtension);
