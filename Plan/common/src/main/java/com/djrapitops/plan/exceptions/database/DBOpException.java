0fb310b68a6465b9422635f5db4eb5ef373b9fe6
Risto Lahtela <24460436+Rsl1122@users.noreply.github.com>
2020-10-06 10:33:28 +0300

    Added error context to errorcode 1267
    Affects issues:
    - Close #1585

--- Plan/common/src/main/java/com/djrapitops/plan/exceptions/database/DBOpException.java
+++ Plan/common/src/main/java/com/djrapitops/plan/exceptions/database/DBOpException.java
@@ -136,6 +136,7 @@ public class DBOpException extends IllegalStateException implements ExceptionWit
             case 1213:
                 context.related("Deadlock");
                 break;
+            case 1267:
             case 1366:
                 context.related("Incorrect character encoding in MySQL")
                         .whatToDo("Convert your MySQL database and tables to use uft8mb4: https://www.a2hosting.com/kb/developer-corner/mysql/convert-mysql-database-utf-8");
