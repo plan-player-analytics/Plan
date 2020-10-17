7569e417ab558a2f52295fb04ec88f2debd25873
FluxCapacitor <31071265+FluxCapacitor2@users.noreply.github.com>
2020-10-17 07:27:16 -0400

    Update to FullCalendar 5 (#1611)

--- Plan/common/src/main/resources/assets/plan/web/js/color-selector.js
+++ Plan/common/src/main/resources/assets/plan/web/js/color-selector.js
@@ -19,6 +19,7 @@
 
     // Function for changing color
     function setColor(nextColor) {
+        $('body').removeClass('theme-' + currentColor).addClass('theme-' + nextColor);
         if (!nextColor || nextColor == currentColor) {
             return;
         }
@@ -218,6 +219,7 @@
                 '.collapse-item:hover,.nav-link.active {background-color: #606270 !important;}' +
                 '.nav-tabs .nav-link.active {background-color: #44475a !important;border-color:#6272a4 #6272a4 #44475a !important;}' +
                 '.fc-today {background:#646e8c !important}' +
+                '.fc-popover-body,.fc-popover-header {background-color: #44475a;color: #eee8d5;}' +
                 nightModeColors +
                 '</style>');
             // Turn bright tables to dark
--- Plan/common/src/main/resources/assets/plan/web/js/sb-admin-2.js
+++ Plan/common/src/main/resources/assets/plan/web/js/sb-admin-2.js
@@ -56,7 +56,7 @@ $('.nav-tabs a.nav-link').click(event => {
     const uriHash = (window.location.hash).split("&");
     if (!uriHash) return;
     const currentTab = uriHash[0];
-    const originalTargetId = event.target.href.split('#')[1];
+    const originalTargetId = event.currentTarget.href.split('#')[1];
     window.location.hash = currentTab + '&' + originalTargetId;
 });
 
@@ -68,16 +68,16 @@ function reduceSidebar() {
         return;
     }
 
-    const $sidebar = $('.sidebar');
+    const $body = $('body')
     const closeModal = $('.sidebar-close-modal');
     if ($(window).width() < 1350) {
-        if (!$sidebar.hasClass('hidden')) $sidebar.addClass('hidden');
+        if (!$body.hasClass('sidebar-hidden')) $body.addClass('sidebar-hidden');
         if (!closeModal.hasClass('hidden')) closeModal.addClass('hidden');
 
         // Close any open menu accordions when window is resized
         $('.sidebar .collapse').collapse('hide');
-    } else if ($(window).width() > 1400 && $sidebar.hasClass('hidden')) {
-        $sidebar.removeClass('hidden');
+    } else if ($(window).width() > 1400 && $body.hasClass('sidebar-hidden')) {
+        $body.removeClass('sidebar-hidden');
         if (!closeModal.hasClass('hidden')) closeModal.addClass('hidden');
     }
     oldWidth = newWidth;
@@ -87,7 +87,7 @@ reduceSidebar();
 $(window).resize(reduceSidebar);
 
 function toggleSidebar() {
-    $('.sidebar').toggleClass('hidden');
+    $('body').toggleClass('sidebar-hidden');
     $('.sidebar .collapse').collapse('hide');
 
     const closeModal = $('.sidebar-close-modal');
