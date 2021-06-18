eb885d09b9e726135bccac8b15808800b8613a24
AuroraLS3 <AuroraLS3@users.noreply.github.com>
2021-06-18 14:30:10 +0000

    5.4.1330

---
+++ Plan/src/main/resources/assets/plan/plan/web/js/loadErrors.js
@@ -0,0 +1,36 @@
+const errorLogs = document.getElementById('error-logs');
+
+function renderErrorLog(i, errorLog) {
+    return createErrorAccordionTitle(i, errorLog) + createErrorAccordionBody(i, errorLog);
+}
+
+function createErrorAccordionTitle(i, errorLog) {
+    let style = 'bg-amber-outline';
+    const tagLine = errorLog.contents[0];
+    return `<tr id="error_h_${i}" aria-controls="error_t_${i}" aria-expanded="false" 
+                class="clickable collapsed ${style}" data-target="#error_t_${i}" data-toggle="collapse">
+                <td>${errorLog.fileName}</td>
+                <td>${tagLine}</td>
+            </tr>`
+}
+
+function createErrorAccordionBody(i, errorLog) {
+    return `<tr class="collapse" data-parent="#tableAccordion" id="error_t_${i}">
+                <td colspan="2">
+                    <pre class="pre-scrollable" style="overflow-x: scroll">${errorLog.contents.join('\n')}</pre>
+                </td>
+            </tr>`;
+}
+
+jsonRequest("./v1/errors", (json, error) => {
+    if (error) {
+        return errorLogs.innerText = `Failed to load /v1/errors: ${error}`;
+    }
+
+    let html = ``;
+    for (let i = 0; i < json.length; i++) {
+        html += renderErrorLog(i, json[i]);
+    }
+
+    errorLogs.innerHTML = html;
+})
