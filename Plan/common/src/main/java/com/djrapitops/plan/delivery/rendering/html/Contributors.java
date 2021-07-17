3a57437c5829df68e652a574755763a5f2304737
HexedHero <6012891+HexedHero@users.noreply.github.com>
2021-07-17 19:59:41 +0100

    Add myself

--- Plan/common/src/main/java/com/djrapitops/plan/delivery/rendering/html/Contributors.java
+++ Plan/common/src/main/java/com/djrapitops/plan/delivery/rendering/html/Contributors.java
@@ -85,6 +85,7 @@ public class Contributors {
             new Contributor("mbax", CODE),
             new Contributor("rymiel", CODE),
             new Contributor("Perchun_Pak", LANG)
+            new Contributor("HexedHero", CODE)
     };
 
     private Contributors() {
