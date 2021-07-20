877647507ccf5063c1de1eb69d8c8e78f36545f7
Antti Koponen <koponen942@outlook.com>
2021-07-20 16:41:25 +0300

    Bind remaining tasks

--- Plan/fabric/src/main/java/net/playeranalytics/plan/modules/fabric/FabricTaskModule.java
+++ Plan/fabric/src/main/java/net/playeranalytics/plan/modules/fabric/FabricTaskModule.java
@@ -17,8 +17,17 @@
 package net.playeranalytics.plan.modules.fabric;
 
 import com.djrapitops.plan.TaskSystem;
+import com.djrapitops.plan.delivery.web.ResourceWriteTask;
+import com.djrapitops.plan.delivery.webserver.cache.JSONFileStorage;
+import com.djrapitops.plan.extension.ExtensionServerDataUpdater;
+import com.djrapitops.plan.gathering.ShutdownDataPreservation;
+import com.djrapitops.plan.gathering.ShutdownHook;
 import com.djrapitops.plan.gathering.timed.ServerTPSCounter;
 import com.djrapitops.plan.gathering.timed.SystemUsageBuffer;
+import com.djrapitops.plan.settings.upkeep.ConfigStoreTask;
+import com.djrapitops.plan.storage.upkeep.DBCleanTask;
+import com.djrapitops.plan.storage.upkeep.LogsFolderCleanTask;
+import com.djrapitops.plan.storage.upkeep.OldDependencyCacheDeletionTask;
 import dagger.Binds;
 import dagger.Module;
 import dagger.multibindings.IntoSet;
@@ -38,10 +47,45 @@ public interface FabricTaskModule {
 
     @Binds
     @IntoSet
+    TaskSystem.Task bindExtensionServerDataUpdater(ExtensionServerDataUpdater extensionServerDataUpdater);
+
+    @Binds
+    @IntoSet
+    TaskSystem.Task bindLogCleanTask(LogsFolderCleanTask logsFolderCleanTask);
+
+    @Binds
+    @IntoSet
+    TaskSystem.Task bindConfigStoreTask(ConfigStoreTask configStoreTask);
+
+    @Binds
+    @IntoSet
+    TaskSystem.Task bindDBCleanTask(DBCleanTask cleanTask);
+
+    @Binds
+    @IntoSet
     TaskSystem.Task bindRamAndCpuTask(SystemUsageBuffer.RamAndCpuTask ramAndCpuTask);
 
     @Binds
     @IntoSet
     TaskSystem.Task bindDiskTask(SystemUsageBuffer.DiskTask diskTask);
 
+    @Binds
+    @IntoSet
+    TaskSystem.Task bindShutdownHookRegistration(ShutdownHook.Registrar registrar);
+
+    @Binds
+    @IntoSet
+    TaskSystem.Task bindJSONFileStorageCleanTask(JSONFileStorage.CleanTask cleanTask);
+
+    @Binds
+    @IntoSet
+    TaskSystem.Task bindShutdownDataPreservation(ShutdownDataPreservation dataPreservation);
+
+    @Binds
+    @IntoSet
+    TaskSystem.Task bindOldDependencyCacheDeletion(OldDependencyCacheDeletionTask deletionTask);
+
+    @Binds
+    @IntoSet
+    TaskSystem.Task bindResourceWriteTask(ResourceWriteTask resourceWriteTask);
 }
