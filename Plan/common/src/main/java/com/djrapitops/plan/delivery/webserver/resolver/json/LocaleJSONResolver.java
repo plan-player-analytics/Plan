b2b2f6e4c6bd303ebb21d2bd668f211876d16aa4
Antti Koponen <koponen942@outlook.com>
2021-10-31 17:47:19 +0200

    Rewrite locale resolver & add language information (current/available)

--- Plan/common/src/main/java/com/djrapitops/plan/delivery/webserver/resolver/json/LocaleJSONResolver.java
+++ Plan/common/src/main/java/com/djrapitops/plan/delivery/webserver/resolver/json/LocaleJSONResolver.java
@@ -19,7 +19,6 @@ package com.djrapitops.plan.delivery.webserver.resolver.json;
 import com.djrapitops.plan.delivery.web.resolver.NoAuthResolver;
 import com.djrapitops.plan.delivery.web.resolver.Response;
 import com.djrapitops.plan.delivery.web.resolver.ResponseBuilder;
-import com.djrapitops.plan.delivery.web.resolver.exception.NotFoundException;
 import com.djrapitops.plan.delivery.web.resolver.request.Request;
 import com.djrapitops.plan.delivery.webserver.Addresses;
 import com.djrapitops.plan.settings.config.Config;
@@ -27,7 +26,6 @@ import com.djrapitops.plan.settings.config.ConfigNode;
 import com.djrapitops.plan.settings.config.ConfigReader;
 import com.djrapitops.plan.settings.locale.LangCode;
 import com.djrapitops.plan.settings.locale.Locale;
-import com.djrapitops.plan.storage.file.FileResource;
 import com.djrapitops.plan.storage.file.PlanFiles;
 import com.djrapitops.plan.storage.file.Resource;
 
@@ -35,12 +33,15 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
 
 /**
- * Resolves JSON requests for /v1/locale.
+ * Resolves JSON requests for /v1/locale and /v1/locale/{@link LangCode#toString()}.
+ *
+ * @author Kopo942
  */
 @Singleton
 public class LocaleJSONResolver implements NoAuthResolver {
@@ -66,49 +67,61 @@ public class LocaleJSONResolver implements NoAuthResolver {
     }
 
     private Response getResponse(Request request) {
-        String langCode = request.getQuery().get("lang").orElse("default");
         ResponseBuilder builder = Response.builder();
+        String path = request.omitFirstInPath().getPath().asString().replaceAll("^/|/$", "");
+        Map<String, Object> json = "".equals(path) ? getLanguageListJSON() : getLocaleJSON(path);
 
-        try {
-            return builder.setJSONContent(getLocaleJSON(langCode)).build();
-        } catch (NotFoundException e) {
+        if (!json.isEmpty()) {
+            return builder.setJSONContent(json).build();
+        } else {
             String address = addresses.getMainAddress().orElse(addresses.getFallbackLocalhostAddress());
             return builder.setStatus(404)
                     .setJSONContent("{\n" +
-                            "  \"message\": \"Language code not found, see " + address + "/v1/language for available codes.\"\n" +
+                            "  \"message\": \"Language not found, see " + address +
+                            "/v1/locale for available language codes.\"\n" +
                             "}")
                     .build();
         }
     }
 
-    private Map<String, Object> getLocaleJSON(String langCode) throws NotFoundException {
-        Map<String, Object> map = new TreeMap<>();
-        Config loadedLocale;
+    private Map<String, Object> getLanguageListJSON() {
+        Map<String, Object> json = new HashMap<>();
+        Map<String, Object> languages = new TreeMap<>();
 
-        if ("default".equals(langCode)) {
-            LangCode currentLang = locale.getLangCode();
-
-            if (LangCode.CUSTOM.equals(currentLang)) {
-                loadedLocale = loadLocale(new FileResource("locale.yml", files.getFileFromPluginFolder("locale.yml")));
-            } else {
-                loadedLocale = loadLocale(files.getResourceFromJar("locale/" + currentLang.getFileName()));
-            }
-        } else {
-            try {
-                LangCode code = LangCode.valueOf(langCode.toUpperCase());
-                loadedLocale = loadLocale(files.getResourceFromJar("locale/" + code.getFileName()));
-            } catch (IllegalArgumentException e) {
-                throw new NotFoundException("Language code not found");
-            }
+        for (LangCode lang : LangCode.values()) {
+            if (lang == LangCode.CUSTOM && locale.getLangCode() != LangCode.CUSTOM) continue;
+            languages.put(lang.toString(), lang.getName());
         }
-        return dfs(loadedLocale, map);
+        json.put("defaultLanguage", locale.getLangCode().toString());
+        json.put("languages", languages);
+
+        return json;
     }
 
-    private Config loadLocale(Resource resource) {
+    private Map<String, Object> getLocaleJSON(String langCode) {
+        LangCode code = LangCode.valueOf(langCode.toUpperCase());
+        Map<String, Object> json = new TreeMap<>();
+        Resource file;
+
+        if (code == LangCode.CUSTOM) {
+            if (locale.getLangCode() != LangCode.CUSTOM || !files.getFileFromPluginFolder("locale.yml").exists()) {
+                return json;
+            }
+            file = files.getResourceFromPluginFolder("locale.yml");
+        } else {
+            file = files.getResourceFromJar("locale/" + code.getFileName());
+        }
+
+        try {
+            return dfs(loadLocale(file), json);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private Config loadLocale(Resource resource) throws IOException {
         try (ConfigReader reader = new ConfigReader(resource.asInputStream())) {
             return reader.read();
-        } catch (IOException e) {
-            throw new UncheckedIOException("Failed to read " + resource.getResourceName(), e);
         }
     }
 
