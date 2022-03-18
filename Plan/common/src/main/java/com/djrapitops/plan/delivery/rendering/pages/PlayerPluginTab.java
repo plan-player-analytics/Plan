50f68f527da414a678eca5dd7b331554efa54bef
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2022-03-18 21:52:56 +0200

    Fixed container fluid issues on network html

--- Plan/common/src/main/java/com/djrapitops/plan/delivery/rendering/pages/PlayerPluginTab.java
+++ Plan/common/src/main/java/com/djrapitops/plan/delivery/rendering/pages/PlayerPluginTab.java
@@ -124,23 +124,13 @@ public class PlayerPluginTab implements Comparable<PlayerPluginTab> {
     }
 
     private String wrapInWideTab(String serverName, String content) {
-        return "<div class=\"tab\" id=\"" + NavLink.format("plugins-" + serverName) + "\"><div class=\"container-fluid mt-4\">" +
-                // Page heading
-                "<div class=\"d-sm-flex align-items-center justify-content-between mb-4\">" +
-                "<h1 class=\"h3 mb-0 text-gray-800\"><i class=\"sidebar-toggler fa fa-fw fa-bars\"></i>" + serverName + " &middot; Plugins Overview</h1>${backButton}" +
-                "</div>" +
-                // End Page heading
-                "<div class=\"row\"><div class=\"col-md-12\">" + content + "</div></div></div></div>";
+        return "<div class=\"tab\" id=\"" + NavLink.format("plugins-" + serverName) + "\">" +
+                "<div class=\"row\"><div class=\"col-md-12\">" + content + "</div></div></div>";
     }
 
     private String wrapInCardColumnsTab(String serverName, String content) {
-        return "<div class=\"tab\" id=\"" + NavLink.format("plugins-" + serverName) + "\"><div class=\"container-fluid mt-4\">" +
-                // Page heading
-                "<div class=\"d-sm-flex align-items-center justify-content-between mb-4\">" +
-                "<h1 class=\"h3 mb-0 text-gray-800\"><i class=\"sidebar-toggler fa fa-fw fa-bars\"></i>" + serverName + " &middot; Plugins Overview</h1>${backButton}" +
-                "</div>" +
-                // End Page heading
-                "<div class=\"row\" data-masonry='{\"percentPosition\": true}'>" + content + "</div></div></div>";
+        return "<div class=\"tab\" id=\"" + NavLink.format("plugins-" + serverName) + "\">" +
+                "<div class=\"row\" data-masonry='{\"percentPosition\": true}'>" + content + "</div></div>";
     }
 
     private TabsElement.Tab wrapToTabElementTab(ExtensionTabData tabData) {
