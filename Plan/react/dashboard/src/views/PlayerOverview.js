b52f257f3c5766aaa7d32b97dd5c8a0f56c4bc35
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2022-05-21 09:33:03 +0300

    Insert html into nickname list on React version of player page
    Affects issues:
    - Fixed #2329

--- Plan/react/dashboard/src/views/PlayerOverview.js
+++ Plan/react/dashboard/src/views/PlayerOverview.js
@@ -12,7 +12,6 @@ import {
     faLocationArrow,
     faServer,
     faSignal,
-    faSignature,
     faSkull,
     faUser,
     faUserPlus,
@@ -27,6 +26,7 @@ import {useTheme} from "../hooks/themeHook";
 import {usePlayer} from "./PlayerPage";
 import {useMetadata} from "../hooks/metadataHook";
 import {useTranslation} from "react-i18next";
+import NicknamesCard from "../components/cards/player/NicknamesCard";
 
 const PlayerOverviewCard = ({player}) => {
     const {t} = useTranslation();
@@ -136,38 +136,6 @@ const PlayerOverviewCard = ({player}) => {
                     </Col>
                 </Row>
             </Card.Body>
-        </Card>
-    );
-}
-
-const NicknamesCard = ({player}) => {
-    const {t} = useTranslation();
-    const {nightModeEnabled} = useTheme();
-    return (
-        <Card>
-            <Card.Header>
-                <h6 className="col-black">
-                    <Fa icon={faSignature}/> {t('html.label.seenNicknames')}
-                </h6>
-            </Card.Header>
-            <Scrollable>
-                <table className={"table table-striped mb-0" + (nightModeEnabled ? " table-dark" : '')}>
-                    <thead className="bg-purple">
-                    <tr>
-                        <th><Fa icon={faSignature}/> {t('html.label.nickname')}</th>
-                        <th><Fa icon={faServer}/> {t('html.label.server')}</th>
-                        <th><Fa icon={faClock}/> {t('html.label.lastSeen')}</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {player.nicknames.map((nickname, i) => (<tr key={'nick-' + i}>
-                        <td>{nickname.nickname}</td>
-                        <td>{nickname.server}</td>
-                        <td>{nickname.date}</td>
-                    </tr>))}
-                    </tbody>
-                </table>
-            </Scrollable>
         </Card>
     );
 }
