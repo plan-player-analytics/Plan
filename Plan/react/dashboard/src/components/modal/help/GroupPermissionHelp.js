876098edd72ae945dad534521509bfcbf16d7ecd
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2023-10-01 08:06:33 +0300

    Fix locale issues with Group permission help modal

--- Plan/react/dashboard/src/components/modal/help/GroupPermissionHelp.js
+++ Plan/react/dashboard/src/components/modal/help/GroupPermissionHelp.js
@@ -9,45 +9,70 @@ const GroupPermissionHelp = () => {
     return (
         <div className={"group-help"}>
             <p><Trans i18nKey="html.label.help.manage.groups.line-1"/></p>
-            <p><Trans i18nKey="html.label.help.manage.groups.line-2" values={{
-                command: <code>/{mainCommand} register</code>,
-                permission: <code>{"plan.webgroup.{group_name}"}</code>
-            }}/></p>
+            <p><Trans i18nKey="html.label.help.manage.groups.line-2"
+                      shouldUnescape={true}
+                      values={{
+                          command: `/${mainCommand} register`,
+                          permission: "plan.webgroup.{group_name}"
+                      }}
+                      components={{1: <code/>, 2: <code/>}}
+            /></p>
             <p><Trans i18nKey="html.label.help.manage.groups.line-3"
-                      values={{command: <code>/{mainCommand} setgroup {"{username} {group_name}"}</code>}}/></p>
-            <p><Trans i18nKey="html.label.help.manage.groups.line-4" values={{
-                icon: <FontAwesomeIcon icon={faExclamationTriangle}/>,
-                permission: <i>manage.groups</i>,
-                commands: <code>/{mainCommand} reload</code>
-            }}/></p>
+                      shouldUnescape={true}
+                      values={{command: `/${mainCommand} setgroup {username} {group_name}`}}
+                      components={{1: <code/>}}
+            /></p>
+            <p><Trans i18nKey="html.label.help.manage.groups.line-4"
+                      shouldUnescape={true}
+                      values={{
+                          icon: '',
+                          permission: "manage.groups",
+                          command: `/${mainCommand} reload`,
+                      }}
+                      components={{1: <FontAwesomeIcon icon={faExclamationTriangle}/>, 2: <i/>, 3: <code/>}}
+            /></p>
             <hr/>
             <h3><Trans i18nKey="html.label.help.manage.groups.line-5"/></h3>
             <p><Trans i18nKey="html.label.help.manage.groups.line-6"
-                      values={{permission1: <i>page.network</i>, permission2: <i>page.network.overview</i>}}/></p>
+                      values={{permission1: "page.network", permission2: "page.network.overview"}}
+                      components={{1: <i/>, 2: <i/>}}
+            /></p>
             <hr/>
             <h3><Trans i18nKey="html.label.help.manage.groups.line-7"/></h3>
             <p><Trans i18nKey="html.label.help.manage.groups.line-8"/></p>
             <ul>
                 <li>
                     <Trans i18nKey="html.label.help.manage.groups.line-9"
-                           values={{permission1: <i>access</i>, permission2: <i>access.network</i>}}/>
+                           values={{permission1: "access", permission2: "access.network"}}
+                           components={{1: <i/>, 2: <i/>}}
+                    />
                 </li>
-                <li><Trans i18nKey="html.label.help.manage.groups.line-10" values={{permission: <i>page</i>}}/></li>
-                <li><Trans i18nKey="html.label.help.manage.groups.line-11" values={{
-                    permission1: <i>access</i>,
-                    permission2: <i>page.network.overview.numbers</i>,
-                    permission3: <i>access.network</i>
-                }}/></li>
+                <li><Trans i18nKey="html.label.help.manage.groups.line-10"
+                           values={{permission: "page"}}
+                           components={{1: <i/>}}
+                /></li>
+                <li><Trans i18nKey="html.label.help.manage.groups.line-11"
+                           values={{
+                               permission1: "access",
+                               permission2: "page.network.overview.numbers",
+                               permission3: "access.network"
+                           }}
+                           components={{1: <i/>, 2: <i/>, 3: <i/>}}
+                /></li>
             </ul>
             <hr/>
             <h3><Trans i18nKey="html.label.help.manage.groups.line-12"/></h3>
             <p><Trans i18nKey="html.label.help.manage.groups.line-13"/></p>
             <p><Trans i18nKey="html.label.help.manage.groups.line-14"/></p>
             <hr/>
-            <p><Trans i18nKey="html.label.help.manage.groups.line-15" values={{
-                link: <a rel="noopener noreferrer" target="_blank"
-                         href={"https://github.com/plan-player-analytics/Plan/wiki/Web-permissions"}>https://github.com/plan-player-analytics/Plan/wiki/Web-permissions</a>
-            }}/></p>
+            <p><Trans i18nKey="html.label.help.manage.groups.line-15"
+                      shouldUnescape={true}
+                      values={{link: "https://github.com/plan-player-analytics/Plan/wiki/Web-permissions"}}
+                      components={{
+                          1: <a rel="noopener noreferrer" target="_blank"
+                                href={"https://github.com/plan-player-analytics/Plan/wiki/Web-permissions"}/>
+                      }}
+            /></p>
         </div>
     )
 };
