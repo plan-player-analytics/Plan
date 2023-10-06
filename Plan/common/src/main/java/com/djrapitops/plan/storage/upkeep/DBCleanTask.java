8eb4a54e6eae09bec6a2676fb0ad5d0c79fa8736
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2023-10-06 08:39:24 +0300

    Disable database player clean on game servers when proxy is installed
    Affects issues:
    - Close #3241

--- Plan/common/src/main/java/com/djrapitops/plan/storage/upkeep/DBCleanTask.java
+++ Plan/common/src/main/java/com/djrapitops/plan/storage/upkeep/DBCleanTask.java
@@ -161,15 +161,21 @@ public class DBCleanTask extends TaskSystem.Task {
 
     // VisibleForTesting
     public int cleanOldPlayers(Database database) {
-        long now = System.currentTimeMillis();
-        long keepActiveAfter = now - config.get(TimeSettings.DELETE_INACTIVE_PLAYERS_AFTER);
+        // Only clean if this is a proxy server or no proxy servers are installed.
+        if (serverInfo.getServer().isProxy() || database.query(ServerQueries.fetchProxyServers()).isEmpty()) {
+            long now = System.currentTimeMillis();
+            long keepActiveAfter = now - config.get(TimeSettings.DELETE_INACTIVE_PLAYERS_AFTER);
 
-        List<UUID> inactivePlayers = database.query(fetchInactivePlayerUUIDs(keepActiveAfter));
-        for (UUID playerUUID : inactivePlayers) {
-            queryService.playerRemoved(playerUUID);
-            database.executeTransaction(new RemovePlayerTransaction(playerUUID));
+            List<UUID> inactivePlayers = database.query(fetchInactivePlayerUUIDs(keepActiveAfter));
+            for (UUID playerUUID : inactivePlayers) {
+                queryService.playerRemoved(playerUUID);
+                database.executeTransaction(new RemovePlayerTransaction(playerUUID));
+            }
+            return inactivePlayers.size();
         }
-        return inactivePlayers.size();
+
+        // Skip cleaning on game servers if proxy server is installed.
+        return 0;
     }
 
     private Query<List<UUID>> fetchInactivePlayerUUIDs(long keepActiveAfter) {
