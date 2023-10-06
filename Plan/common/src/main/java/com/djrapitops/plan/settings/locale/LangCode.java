96f49c0504176943586e3bd06b32560783705354
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2023-10-06 14:28:15 +0300

    Update Plan/common/src/main/java/com/djrapitops/plan/settings/locale/LangCode.java

--- Plan/common/src/main/java/com/djrapitops/plan/settings/locale/LangCode.java
+++ Plan/common/src/main/java/com/djrapitops/plan/settings/locale/LangCode.java
@@ -26,7 +26,7 @@ public enum LangCode {
     CUSTOM("Custom", ""),
     EN("English", "AuroraLS3"),
     ES("Español", "Catalina, itaquito, Elguerrero & 4drian3d"),
-    CN("\u6C49\u8BED", "f0rb1d (\u4f5b\u58c1\u706f), qsefthuopq, shaokeyibb, Fur_xia, 10935336, SkipM4 & TheLittle_Yang, jhqwqmc"), // Simplified Chinese
+    CN("\u6C49\u8BED", "f0rb1d (\u4f5b\u58c1\u706f), qsefthuopq, shaokeyibb, Fur_xia, 10935336, SkipM4,  TheLittle_Yang & jhqwqmc"), // Simplified Chinese
     CS("čeština", "Shadowhackercz, QuakyCZ, MrFriggo & WolverStones"),
     DE("Deutsch", "Eyremba, fuzzlemann, Morsmorse, hallo1142 & DubHacker"),
     FI("suomi", "AuroraLS3, KasperiP"),
