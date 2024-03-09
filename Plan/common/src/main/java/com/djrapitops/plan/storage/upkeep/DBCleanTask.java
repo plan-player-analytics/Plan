a862fcb31a733a35df2f975758736dd66702c8cd
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2024-03-09 14:05:28 +0200

    Remove deprecated code
    - RemoveUnsatisfiedConditionalPlayerResultsTransaction.java
    - RemoveUnsatisfiedConditionalServerResultsTransaction.java

--- Plan/common/src/main/java/com/djrapitops/plan/storage/upkeep/DBCleanTask.java
+++ Plan/common/src/main/java/com/djrapitops/plan/storage/upkeep/DBCleanTask.java
@@ -110,8 +110,6 @@ public class DBCleanTask extends TaskSystem.Task {
                         config.get(TimeSettings.DELETE_PING_DATA_AFTER)
                 ));
                 database.executeTransaction(new RemoveDuplicateUserInfoTransaction());
-//                database.executeTransaction(new RemoveUnsatisfiedConditionalPlayerResultsTransaction());
-//                database.executeTransaction(new RemoveUnsatisfiedConditionalServerResultsTransaction());
                 int removed = cleanOldPlayers(database);
                 if (removed > 0) {
                     logger.info(locale.getString(PluginLang.DB_NOTIFY_CLEAN, removed));
