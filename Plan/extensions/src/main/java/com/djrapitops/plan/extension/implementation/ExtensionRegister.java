9282530da18c24f6c53ad41d375d7ab0fccf03d2
Yannic Hock <yannic.hock@dfki.de>
2024-10-19 14:47:48 +0200

    Removed seconds Extension for CMI as it now uses Tabs

--- Plan/extensions/src/main/java/com/djrapitops/plan/extension/implementation/ExtensionRegister.java
+++ Plan/extensions/src/main/java/com/djrapitops/plan/extension/implementation/ExtensionRegister.java
@@ -31,7 +31,7 @@ import net.playeranalytics.extension.discordsrv.DiscordSRVExtensionFactory;
 import net.playeranalytics.extension.dkbans.DKBansExtensionFactory;
 import net.playeranalytics.extension.dkcoins.DKCoinsExtensionFactory;
 import net.playeranalytics.extension.essentials.EssentialsExtensionFactory;
-import net.playeranalytics.extension.CMI.CMIExtensionFactory;
+import net.playeranalytics.extension.cmi.CMIExtensionFactory;
 import net.playeranalytics.extension.factions.FactionsExtensionFactory;
 import net.playeranalytics.extension.factionsuuid.FactionsUUIDExtensionFactory;
 import net.playeranalytics.extension.fastlogin.FastLoginExtensionFactory;
@@ -114,7 +114,6 @@ public class ExtensionRegister {
         register(new EssentialsExtensionFactory(), EssentialsExtensionFactory::createExtension, EssentialsExtensionFactory::registerUpdateListeners);
         register(new EssentialsExtensionFactory(), EssentialsExtensionFactory::createEcoExtension, EssentialsExtensionFactory::registerEconomyUpdateListeners);
         register(new CMIExtensionFactory(), CMIExtensionFactory::createCMIExtension, CMIExtensionFactory::registerCMIUpdateListeners);
-        register(new CMIExtensionFactory(), CMIExtensionFactory::createCMIEcoExtension, CMIExtensionFactory::registerEcoUpdateListeners);
         register(new FactionsExtensionFactory(), FactionsExtensionFactory::createExtension);
         register(new FactionsUUIDExtensionFactory(), FactionsUUIDExtensionFactory::createExtension, FactionsUUIDExtensionFactory::registerExpansion);
         register(new FastLoginExtensionFactory(), FastLoginExtensionFactory::createExtension);
