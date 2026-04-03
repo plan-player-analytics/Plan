7141e8c416c4f9fc96e7134d8b70857def62ec8b
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2026-04-03 12:35:55 +0300

    Fix MSPT not showing if under 1ms

--- Plan/react/dashboard/src/util/format/useDecimalFormatter.js
+++ Plan/react/dashboard/src/util/format/useDecimalFormatter.js
@@ -6,7 +6,7 @@ export const useDecimalFormatter = () => {
     const {decimalFormat} = usePreferences();
 
     const formatDecimals = useCallback((value) => {
-        if (!decimalFormat || Number.isNaN(value) || isString(value)) return value;
+        if (!decimalFormat || Number.isNaN(Number(value)) || isString(value)) return value;
         const split = decimalFormat.includes('.') ? decimalFormat.split('.') : decimalFormat.split(',');
         if (split.length <= 1) return value.toFixed(0);
         return value.toFixed(split[1].length);
--- Plan/react/dashboard/src/util/format/usePingFormatter.js
+++ Plan/react/dashboard/src/util/format/usePingFormatter.js
@@ -1,31 +1,18 @@
 import {isNumber} from "../isNumber.js";
 import {useCallback, useMemo} from "react";
 import {useI18nFriendlyLanguage} from "../../service/localeService.js";
+import {useDecimalFormatter} from "./useDecimalFormatter.js";
 
 export const usePingFormatter = () => {
     const locale = useI18nFriendlyLanguage();
+    const {formatDecimals} = useDecimalFormatter();
     const formatter = useMemo(() => {
         return new Intl.DurationFormat(locale, {style: 'narrow'})
     }, [locale]);
 
     const formatPing = useCallback((value) => {
         if (isNumber(value) && !String(value).includes("ms")) {
-            const split = isNumber(value) ? [value] : value.split('.');
-            const decimals = split.length > 1 ? split[1] : value % 1;
-            const parts = formatter.formatToParts({milliseconds: Math.floor(Number(split[0]))});
-
-            const ints = parts.map((part, i) => part.type === 'integer' ? i : -1)
-                .filter((i) => i >= 0);
-            const lastIndex = ints[ints.length - 1];
-            return parts.map((part, i) => {
-                if (part.type === 'group') {
-                    return '';
-                }
-                if (lastIndex === i && split.length > 1) {
-                    return isNumber(decimals) ? Number(part.value) + decimals : part.value + '.' + decimals;
-                }
-                return part.value;
-            }).join('');
+            return formatter.format({milliseconds: 1}).replaceAll("1", formatDecimals(value))
         }
         return value;
     }, [formatter]);
